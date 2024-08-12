788f0c3ceb2064f5f79378aa855f1a5e0704b0f9
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-12 07:58:24 -0500

    feat(takane.hyprland): change keyboard layout order for takane

--- hosts/nitori/home-manager/home.nix
+++ hosts/nitori/home-manager/home.nix
@@ -1,6 +1,10 @@
 {
-  wayland.windowManager.hyprland.settings.monitor = [
-    ", preferred, auto, 1.2"
-    "HDMI-A-1, preferred, auto-left, 1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      ", preferred, auto, 1.2"
+      "HDMI-A-1, preferred, auto-left, 1"
+    ];
+
+    input.kb_layout = "us,latam";
+  };
 }
--- hosts/takane/home-manager/home.nix
+++ hosts/takane/home-manager/home.nix
@@ -1,5 +1,9 @@
 {
-  wayland.windowManager.hyprland.settings.monitor = [
-    ", preferred, auto, 1"
-  ];
+  wayland.windowManager.hyprland.settings = {
+    monitor = [
+      ", preferred, auto, 1"
+    ];
+
+    input.kb_layout = "latam,us";
+  };
 }
