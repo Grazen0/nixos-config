9ac41f3195bdf985ae69843850863242f8a4314f
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-11 22:44:59 -0500

    refactor(hyprland): remove name of main monitor in hyprpaper

--- shared/home-manager/modules/hypr/hyprpaper.nix
+++ shared/home-manager/modules/hypr/hyprpaper.nix
@@ -7,7 +7,7 @@
         "~/Pictures/Wallpapers/secondary.png"
       ];
       wallpaper = [
-        "eDP-1,~/Pictures/Wallpapers/primary.png"
+        ",~/Pictures/Wallpapers/primary.png"
         "HDMI-A-1,~/Pictures/Wallpapers/secondary.png"
       ];
     };
