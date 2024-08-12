d1de2952b4d82b5bf6f4e37312fe283eec078642
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-11 22:09:29 -0500

    fix(hyprland): fix some workspace and floating rules

--- shared/home-manager/modules/hypr/hyprland/rules.nix
+++ shared/home-manager/modules/hypr/hyprland/rules.nix
@@ -5,18 +5,18 @@
       "float, ^(org.pulseaudio.pavucontrol)$"
       "float, blueman-manager"
       "float, ^(nm-connection-editor)$"
-      "float, ^(nvidia-settings)$"
 
       "workspace 3, ^(obsidian)$"
       "workspace 6, ^(discord)$"
-      "workspace 10, Spotify"
+      "workspace 10, title:Spotify"
     ];
 
     windowrulev2 = [
       "bordercolor rgb(e6c384), pinned:1"
       "noborder, fullscreen:1"
+      "opacity 0.8, floating:1, focus:0"
+
       "suppressevent maximize, class:.*"
-      "opacity 0.85, floating:1, focus:0"
     ];
   };
 }
