eeae3e113dda757c89c3ab655f84efcc3b3bdea3
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-12 12:53:30 -0500

    feat(nvim): add format enable/disable user commands

--- shared/nixos/modules/nixvim/plugins/conform.nix
+++ shared/nixos/modules/nixvim/plugins/conform.nix
@@ -25,10 +25,15 @@
       nix = ["alejandra"];
     };
 
-    formatOnSave = {
-      lspFallback = true;
-      timeoutMs = 1000;
-    };
+    formatOnSave = ''
+      function(bufnr)
+        -- Disable with a global or buffer-local variable
+        if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+          return;
+        end
+        return { timeout_ms = 1000, lsp_format = "fallback" };
+      end
+    '';
   };
 
   programs.nixvim.extraPackages = with pkgs; [
--- shared/nixos/modules/nixvim/user-commands.nix
+++ shared/nixos/modules/nixvim/user-commands.nix
@@ -4,11 +4,43 @@
 
     Format = {
       command.__raw = ''
-        function()
-        	require('conform').format({ timeout_ms = 500, lsp_fallback = true });
+        function(args)
+          local range = nil;
+          if args.count ~= -1 then
+            local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1];
+            range = {
+              start = { args.line1, 0 },
+              ["end"] = { args.line2, end_line:len() },
+            };
+          end
+          require("conform").format({ async = true, lsp_format = "fallback", range = range });
         end
       '';
       range = true;
+    };
+
+    FormatDisable = {
+      command.__raw = ''
+        function(args)
+          if args.bang then
+            -- FormatDisable! will disable formatting just for this buffer
+            vim.b.disable_autoformat = true;
+          else
+            vim.g.disable_autoformat = true;
+          end
+        end
+      '';
+      bang = true;
+    };
+
+    FormatEnable = {
+      command.__raw = ''
+        function()
+          vim.b.disable_autoformat = false;
+          vim.g.disable_autoformat = false;
+        end
+      '';
+      bang = true;
     };
 
     SessionWrite = {
