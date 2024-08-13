9bed27d615d274aaba749c26fd05c8738198bd63
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-13 09:59:42 -0500

    refactor: add home manager programs directory

--- shared/home-manager/modules/default.nix
+++ shared/home-manager/modules/default.nix
@@ -1,22 +1,12 @@
 {
   imports = [
     ./hypr
+    ./programs
     ./waybar
-    ./bat.nix
-    ./cava.nix
     ./dunst.nix
-    ./fastfetch.nix
-    ./git.nix
     ./gtk-qt.nix
-    ./kitty.nix
-    ./librewolf.nix
-    ./obs-studio.nix
-    ./peaclock.nix
-    ./ranger.nix
     ./rofi.nix
-    ./tmux.nix
     ./xdg.nix
-    ./zathura.nix
     ./zsh.nix
   ];
 }
---
+++ shared/home-manager/modules/programs/default.nix
@@ -0,0 +1,15 @@
+{
+  imports = [
+    ./bat.nix
+    ./cava.nix
+    ./fastfetch.nix
+    ./git.nix
+    ./kitty.nix
+    ./librewolf.nix
+    ./obs-studio.nix
+    ./peaclock.nix
+    ./ranger.nix
+    ./tmux.nix
+    ./zathura.nix
+  ];
+}
