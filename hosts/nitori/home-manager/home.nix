5bea56235378c44acb3e7d7fe8cb8046907492ff
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-13 01:25:35 -0500

    fix(hyprland): fix monitor connect handler script

--- hosts/nitori/home-manager/home.nix
+++ hosts/nitori/home-manager/home.nix
@@ -1,12 +1,19 @@
-{
+{config, ...}: {
+  home.file.handle_monitor_connect = {
+    enable = true;
+    source = ./files/bin;
+    recursive = true;
+    target = "${config.home.homeDirectory}/.local/bin";
+  };
+
   wayland.windowManager.hyprland.settings = {
     monitor = [
       ", preferred, auto, 1.2"
       "HDMI-A-1, preferred, auto-left, 1"
     ];
 
-    # exec-once = [
-    #   ./scripts/handle_monitor_connect
-    # ];
+    exec-once = [
+      "${config.home.homeDirectory}/.local/bin/handle_monitor_connect"
+    ];
   };
 }
