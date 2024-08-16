0af3ada7a4b21417726e8fcace8dcceb40b0b1b5
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-16 09:07:47 -0500

    feat(nvim.neo-tree): don't hide cursor anymore

--- shared/nixos/modules/programs/nixvim/plugins/neo-tree.nix
+++ shared/nixos/modules/programs/nixvim/plugins/neo-tree.nix
@@ -101,28 +101,10 @@
       };
     };
 
-    eventHandlers = {
-      neo_tree_buffer_enter = ''
-        function()
-          vim.opt_local.relativenumber = true;
-
-          -- Hide cursor
-          local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
-          hl.blend = 100
-          vim.api.nvim_set_hl(0, 'Cursor', hl)
-          vim.opt.guicursor:append('a:Cursor/lCursor')
-        end
-      '';
-
-      neo_tree_buffer_leave = ''
-        function()
-          -- Show cursor
-          local hl = vim.api.nvim_get_hl_by_name('Cursor', true)
-          hl.blend = 0
-          vim.api.nvim_set_hl(0, 'Cursor', hl)
-          vim.opt.guicursor:remove('a:Cursor/lCursor')
-        end
-      '';
-    };
+    eventHandlers.neo_tree_buffer_enter = ''
+      function()
+        vim.opt_local.relativenumber = true;
+      end
+    '';
   };
 }
