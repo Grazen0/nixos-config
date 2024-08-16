4b3b19b8ab42a985923956f69e91530cf5a8b015
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-16 07:26:05 -0500

    feat(nvim.neo-tree): add hjkl navigation and file preview with tab

--- shared/nixos/modules/programs/nixvim/plugins/neo-tree.nix
+++ shared/nixos/modules/programs/nixvim/plugins/neo-tree.nix
@@ -6,9 +6,42 @@
       width = 30;
 
       mappings = {
+        # Open directory or preview file with tab
         "<Tab>".__raw = ''
-          function()
-            -- TODO: preview if current node is file, else (if directory) open
+          function(state)
+            local node = state.tree:get_node()
+            if require("neo-tree.utils").is_expandable(node) then
+              state.commands["toggle_node"](state)
+            else
+              state.commands.open(state)
+              vim.cmd("Neotree reveal")
+            end
+          end
+        '';
+
+        # Navigation with HJKL
+        h.__raw = ''
+          function(state)
+            local node = state.tree:get_node();
+            if node.type == "directory" and node:is_expanded() then
+              require("neo-tree.sources.filesystem").toggle_directory(state, node);
+            else
+              require("neo-tree.ui.renderer").focus_node(state, node:get_parent_id());
+            end
+          end
+        '';
+        l.__raw = ''
+          function(state)
+            local node = state.tree:get_node()
+            if node.type == "directory" then
+              if not node:is_expanded() then
+                require("neo-tree.sources.filesystem").toggle_directory(state, node);
+              elseif node:has_children() then
+                require("neo-tree.ui.renderer").focus_node(state, node:get_child_ids()[1]);
+              end
+            else
+              state.commands.open(state);
+            end
           end
         '';
 
