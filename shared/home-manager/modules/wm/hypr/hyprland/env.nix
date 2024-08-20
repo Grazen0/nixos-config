ed3352f985d4ff7d96b5c94a297dd54faefd4120
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-19 22:03:29 -0500

    fix(hyprland): add XCURSOR_THEME env variable for xwayland compatibility

--- shared/home-manager/modules/wm/hypr/hyprland/env.nix
+++ shared/home-manager/modules/wm/hypr/hyprland/env.nix
@@ -4,6 +4,7 @@
       "XDG_CURRENT_DESKTOP,Hyprland"
       "XDG_SESSION_TYPE,wayland"
       "XDG_SESSION_DESKTOP,Hyprland"
+      "XCURSOR_THEME,Bibata-Modern-Ice"
       "XCURSOR_SIZE,24"
       "QT_GPA_PLATFORM,wayland"
     ];
