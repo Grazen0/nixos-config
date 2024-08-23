2023899ac829482891a7221fb514cec9df7f5e18
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-22 22:55:12 -0500

    fix(hyprland): use pkgs.brightnessctl on brightness key binds

--- shared/home-manager/modules/wm/hypr/hyprland/binds.nix
+++ shared/home-manager/modules/wm/hypr/hyprland/binds.nix
@@ -131,8 +131,8 @@
         "$mainMod, mouse_up, workspace, r-1"
 
         # Brightness control
-        ", XF86MonBrightnessUp, exec, brightnessctl set 5%+"
-        ", XF86MonBrightnessDown, exec, brightnessctl set 5%-"
+        ", XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%+"
+        ", XF86MonBrightnessDown, exec, ${pkgs.brightnessctl}/bin/brightnessctl set 5%-"
       ];
 
       binde = [
