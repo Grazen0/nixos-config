775454a259fed5a771d3b77cd25ff27ebb6e4c68
ElCholoGamer <josedanielgrayson@proton.me>
2024-08-28 14:08:50 -0500

    fix: make steam enable just a default

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,4 +1,4 @@
-{
+{lib, ...}: {
   imports = [
     ./nixvim
     ./thunar.nix
@@ -8,6 +8,6 @@
   programs = {
     dconf.enable = true;
     hyprland.enable = true;
-    steam.enable = true;
+    steam.enable = lib.mkDefault true;
   };
 }
