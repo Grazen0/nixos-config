b80f3bcec81de3cf30bdbaa6bbf45c7ad95f44ba
Grazen0 <josedanielgrayson@proton.me>
2024-09-09 15:17:38 -0500

    refactor(rofi): replace bemoji with rofi-emoji

--- modules/home-manager/wm/hypr/hyprland/binds.nix
+++ modules/home-manager/wm/hypr/hyprland/binds.nix
@@ -22,7 +22,7 @@
         # Menus
         "$mainMod, Space, exec, pkill rofi || rofi -show drun -theme kanagawa-grid"
         "$mainMod, V, exec, cliphist list | rofi -dmenu | cliphist decode | wl-copy"
-        "$mainMod, Period, exec, bemoji -tcn"
+        "$mainMod, Period, exec, rofi -modi emoji -show emoji"
         "$mainMod, X, exec, rofi -show menu -modi \"menu:rofi-power-menu\""
         "$mainMod, Slash, exec, rofi -show calc -modi calc -no-show-match -no-sort"
 
--- modules/home-manager/wm/rofi.nix
+++ modules/home-manager/wm/rofi.nix
@@ -10,6 +10,7 @@
 
     plugins = with pkgs; [
       rofi-calc
+      rofi-emoji
     ];
 
     extraConfig = {
