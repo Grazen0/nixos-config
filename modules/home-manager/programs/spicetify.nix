eccd339a384a8eac5f95e5436fcf6c44a2761131
Grazen0 <josedanielgrayson@proton.me>
2024-09-10 16:20:55 -0500

    feat(spicetify): set jetbrains font and change some plugins

--- modules/home-manager/programs/spicetify.nix
+++ modules/home-manager/programs/spicetify.nix
@@ -6,19 +6,28 @@
   inherit (inputs) spicetify-nix;
   spicePkgs = spicetify-nix.legacyPackages.${pkgs.system};
 in {
-  imports = [
-    spicetify-nix.homeManagerModules.default
-  ];
+  imports = [spicetify-nix.homeManagerModules.default];
 
   programs.spicetify = {
     enable = true;
-    theme = spicePkgs.themes.text;
+
+    theme =
+      spicePkgs.themes.text
+      // {
+        additionalCss = ''
+          :root {
+            --font-family: "JetBrainsMonoNL Nerd Font Propo", monospace !important;
+            letter-spacing: -0.075ch !important;
+          }
+        '';
+      };
+
     colorScheme = "kanagawa";
 
     enabledExtensions = with spicePkgs.extensions; [
       adblock
-      hidePodcasts
       shuffle
+      keyboardShortcut
     ];
   };
 }
