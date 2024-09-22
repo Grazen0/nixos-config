1477af34a07aa54bb74f1f1356b186fc39b29d7c
Grazen0 <josedanielgrayson@proton.me>
2024-09-22 12:02:27 -0500

    feat(packages): update zen browser to 1.0.1-a.2

--- modules/home-manager/packages/desktop-apps.nix
+++ modules/home-manager/packages/desktop-apps.nix
@@ -14,7 +14,15 @@
     pavucontrol
     zoom-us
     inputs.thorium-browser.defaultPackage.${system}
-    inputs.zen-browser.packages.${system}.specific
+    (inputs.zen-browser.packages.${system}.specific.overrideAttrs (prev: let
+      version = "1.0.1-a.2";
+    in {
+      inherit version;
+      src = builtins.fetchTarball {
+        url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
+        sha256 = "sha256:0an4i57736scjwlmiqj9w7fh950brkrlg82iyv86m51ycxsi58sk";
+      };
+    }))
 
     # LibreOffice + spellcheck
     libreoffice-qt
