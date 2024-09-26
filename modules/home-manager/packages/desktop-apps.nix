79cad917685a83379ca20fa43d2d264d79e454c6
Grazen0 <josedanielgrayson@proton.me>
2024-09-25 23:59:45 -0500

    deps(zen-browser): update to latest version

--- modules/home-manager/packages/desktop-apps.nix
+++ modules/home-manager/packages/desktop-apps.nix
@@ -13,15 +13,7 @@
     pavucontrol
     zoom-us
     inputs.thorium-browser.defaultPackage.${system}
-    (inputs.zen-browser.packages.${system}.specific.overrideAttrs (prev: let
-      version = "1.0.1-a.4";
-    in {
-      inherit version;
-      src = builtins.fetchTarball {
-        url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-        sha256 = "0jjfr1201gfw0cy8q1jbr504994z33sbw8ip86c6xbww8qm60bqh";
-      };
-    }))
+    inputs.zen-browser.packages.${system}.specific
 
     # LibreOffice + spellcheck
     libreoffice-qt
