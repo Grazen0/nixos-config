79e0c31797c4e3b7fc65b6c67230ecd74997f1c2
Grazen0 <josedanielgrayson@proton.me>
2024-10-03 16:11:07 -0500

    add bufferline

---
+++ modules/home-manager/programs/nvf/plugins/bufferline.nix
@@ -0,0 +1,11 @@
+{
+  programs.nvf.settings.vim.tabline.nvimBufferline = {
+    enable = true;
+
+    setupOpts.options = {
+      numbers = "none";
+    };
+
+    mappings.closeCurrent = "<leader>bd";
+  };
+}
--- modules/home-manager/programs/nvf/plugins/default.nix
+++ modules/home-manager/programs/nvf/plugins/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./bufferline.nix
     ./conform.nix
     ./gitsigns.nix
     ./kanagawa.nix
