18711de525b048ee69d827bb50e05b97f55c1d38
Grazen0 <josedanielgrayson@proton.me>
2024-10-08 10:11:04 -0500

    style(nvf): add stylua and format lua files

--- modules/home-manager/programs/nvf/lua/autocmds.lua
+++ modules/home-manager/programs/nvf/lua/autocmds.lua
@@ -2,35 +2,35 @@ local create_autocmd = vim.api.nvim_create_autocmd
 local create_augroup = vim.api.nvim_create_augroup
 
 -- Restore cursor position on buffer enter
-create_autocmd("BufReadPost", {
+create_autocmd('BufReadPost', {
     command = 'silent! normal g`"zv',
 })
 
 -- Highlight yanked text
-create_augroup("YankHighlight", { clear = true })
-create_autocmd("TextYankPost", {
-    group = "YankHighlight",
+create_augroup('YankHighlight', { clear = true })
+create_autocmd('TextYankPost', {
+    group = 'YankHighlight',
     callback = function()
-        vim.highlight.on_yank({ higroup = "Search", timeout = 500 })
+        vim.highlight.on_yank({ higroup = 'Search', timeout = 500 })
     end,
 })
 
 -- Open help windows in vertical split
-create_autocmd("FileType", {
-    pattern = "help",
-    command = "wincmd L",
+create_autocmd('FileType', {
+    pattern = 'help',
+    command = 'wincmd L',
 })
 
 -- Use relative numbers only on focused buffer
-create_autocmd({ "WinEnter", "FocusGained" }, {
-    pattern = "*",
+create_autocmd({ 'WinEnter', 'FocusGained' }, {
+    pattern = '*',
     callback = function()
         vim.opt_local.number = true
         vim.opt_local.relativenumber = true
     end,
 })
-create_autocmd({ "WinLeave", "FocusLost" }, {
-    pattern = "*",
+create_autocmd({ 'WinLeave', 'FocusLost' }, {
+    pattern = '*',
     callback = function()
         vim.opt_local.number = true
         vim.opt_local.relativenumber = false
@@ -38,17 +38,17 @@ create_autocmd({ "WinLeave", "FocusLost" }, {
 })
 
 -- Show diagnostic window when cursor is over warning/error
-create_augroup("FloatDiagnosticCursor", { clear = true })
-create_autocmd({ "CursorHold", "CursorHoldI" }, {
-    group = "FloatDiagnosticCursor",
+create_augroup('FloatDiagnosticCursor', { clear = true })
+create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+    group = 'FloatDiagnosticCursor',
     callback = function()
-        vim.diagnostic.open_float(nil, { focus = false, scope = "cursor" })
+        vim.diagnostic.open_float(nil, { focus = false, scope = 'cursor' })
     end,
 })
 
 -- Enable spellcheck for LaTeX files
-create_autocmd("FileType", {
-    pattern = { "tex", "plaintex" },
+create_autocmd('FileType', {
+    pattern = { 'tex', 'plaintex' },
     callback = function()
         vim.opt_local.spell = true
         vim.b.disable_autoformat = true
@@ -56,28 +56,28 @@ create_autocmd("FileType", {
 })
 
 -- Conceal level for markdown and LaTeX files
-create_autocmd("FileType", {
-    pattern = { "markdown", "tex", "plaintex" },
+create_autocmd('FileType', {
+    pattern = { 'markdown', 'tex', 'plaintex' },
     callback = function()
         vim.opt_local.conceallevel = 2
     end,
 })
 
 -- Tab width 2 for some filetypes
-create_autocmd("FileType", {
+create_autocmd('FileType', {
     pattern = {
-        "nix",
-        "haskell",
-        "yuck",
-        "html",
-        "css",
-        "scss",
-        "json",
-        "jsonc",
-        "javascript",
-        "javascriptreact",
-        "typescript",
-        "typescriptreact",
+        'nix',
+        'haskell',
+        'yuck',
+        'html',
+        'css',
+        'scss',
+        'json',
+        'jsonc',
+        'javascript',
+        'javascriptreact',
+        'typescript',
+        'typescriptreact',
     },
     callback = function()
         vim.opt_local.shiftwidth = 2
--- modules/home-manager/programs/nvf/lua/user-commands.lua
+++ modules/home-manager/programs/nvf/lua/user-commands.lua
@@ -1,28 +1,36 @@
 local create_user_command = vim.api.nvim_create_user_command
 
 -- I'm kinda clumsy
-create_user_command("W", "w", {})
-create_user_command("Wa", "wa", {})
-create_user_command("Q", "q", {})
-create_user_command("Qa", "qa", {})
+create_user_command('W', 'w', {})
+create_user_command('Wa', 'wa', {})
+create_user_command('Q', 'q', {})
+create_user_command('Qa', 'qa', {})
 
 -- Format bufffer
-create_user_command("Format", function(args)
+create_user_command('Format', function(args)
     local range = nil
     if args.count ~= -1 then
-        local end_line = vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-        range = { start = { args.line1, 0 }, ["end"] = { args.line2, end_line:len() } }
+        local end_line =
+            vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+            start = { args.line1, 0 },
+            ['end'] = { args.line2, end_line:len() },
+        }
     end
-    require("conform").format({ async = true, lsp_format = "fallback", range = range })
+    require('conform').format({
+        async = true,
+        lsp_format = 'fallback',
+        range = range,
+    })
 end, { range = true })
 
 -- Toggle formatting
-create_user_command("FormatEnable", function()
+create_user_command('FormatEnable', function()
     vim.b.disable_autoformat = false
     vim.g.disable_autoformat = false
 end, { bang = true })
 
-create_user_command("FormatDisable", function(args)
+create_user_command('FormatDisable', function(args)
     if args.bang then
         -- FormatDisable! will disable formatting just for this buffer
         vim.b.disable_autoformat = true
@@ -32,21 +40,21 @@ create_user_command("FormatDisable", function(args)
 end, { bang = true })
 
 -- Close all buffers except current
-create_user_command("Zen", "%bd|e#|bd#", {})
+create_user_command('Zen', '%bd|e#|bd#', {})
 
 -- Sessions
-create_user_command("SessionWrite", function(opts)
+create_user_command('SessionWrite', function(opts)
     MiniSessions.write(opts.args)
 end, { nargs = 1 })
 
-create_user_command("SessionDelete", function(opts)
-    if opts.args == "" then
+create_user_command('SessionDelete', function(opts)
+    if opts.args == '' then
         MiniSessions.delete(nil, { force = true })
     else
         MiniSessions.delete(opts.args, { force = true })
     end
 end, {
-    nargs = "?",
+    nargs = '?',
     complete = function()
         local sessions = {}
         for session_name, _ in pairs(MiniSessions.detected) do
