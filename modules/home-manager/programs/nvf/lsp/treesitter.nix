0799ff68dc6e7fc55dc5c3ac9f8fd49e3070a786
Grazen0 <josedanielgrayson@proton.me>
2024-10-09 00:33:33 -0500

    feat(nvf): add treesitter-textobjects + format some random JSON file

--- modules/home-manager/programs/nvf/lsp/treesitter.nix
+++ modules/home-manager/programs/nvf/lsp/treesitter.nix
@@ -37,6 +37,38 @@
           })
         '';
       };
+
+      treesitter-textobjects = {
+        package = vimPlugins.nvim-treesitter-textobjects;
+        setup = ''
+          require('nvim-treesitter.configs').setup({
+            textobjects = {
+              select = {
+                enable = true,
+                keymaps = {
+                  ['aa'] = '@parameter.outer',
+                  ['ia'] = '@parameter.inner',
+                  ['af'] = '@function.outer',
+                  ['if'] = '@function.inner',
+                  ['ac'] = '@class.outer',
+                  ['ic'] = '@class.inner',
+                  ['ai'] = '@conditional.outer',
+                  ['ii'] = '@conditional.inner',
+                  ['al'] = '@loop.outer',
+                  ['il'] = '@loop.inner',
+                  ['ak'] = '@block.outer',
+                  ['ik'] = '@block.inner',
+                  ['is'] = '@statement.inner',
+                  ['as'] = '@statement.outer',
+                  ['ad'] = '@comment.outer',
+                  ['am'] = '@call.outer',
+                  ['im'] = '@call.inner',
+                },
+              },
+            },
+          })
+        '';
+      };
     };
   };
 }
