07d1c82bcfeb9590d8f9ddf6de7737691edacfe8
Grazen0 <josedanielgrayson@proton.me>
2024-10-21 16:15:01 -0500

    refactor: make zsh the default shell only for my user

--- modules/nixos/programs/default.nix
+++ modules/nixos/programs/default.nix
@@ -1,7 +1,6 @@
 {lib, ...}: {
   imports = [
     ./thunar.nix
-    ./zsh.nix
   ];
 
   programs = {
--- modules/nixos/users.nix
+++ modules/nixos/users.nix
@@ -1,6 +1,9 @@
-{
+{pkgs, ...}: {
+  programs.zsh.enable = true;
+
   users.users.jdgt = {
     isNormalUser = true;
     extraGroups = ["wheel" "audio" "networkmanager"];
+    shell = pkgs.zsh;
   };
 }
