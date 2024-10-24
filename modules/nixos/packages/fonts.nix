8d74527f95dfcf47a48a409817bee7280b4ad059
Grazen0 <josedanielgrayson@proton.me>
2024-10-23 19:42:05 -0500

    deps: pin nixpkgs to fix an error + update noto-fonts-cjk package name

--- modules/nixos/packages/fonts.nix
+++ modules/nixos/packages/fonts.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}: {
   fonts.packages = with pkgs; [
     noto-fonts
-    noto-fonts-cjk
+    noto-fonts-cjk-sans
     noto-fonts-emoji
     liberation_ttf
     (nerdfonts.override {fonts = ["JetBrainsMono"];})
