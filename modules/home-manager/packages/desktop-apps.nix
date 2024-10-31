4ed40281f2ab0378c336ad0ac4ae46d383e8d763
Grazen0 <josedanielgrayson@proton.me>
2024-10-31 15:50:37 -0500

    deps: update zen browser to 1.0.1-a.16

--- modules/home-manager/packages/desktop-apps.nix
+++ modules/home-manager/packages/desktop-apps.nix
@@ -14,12 +14,12 @@
     zoom-us
     inputs.thorium-browser.defaultPackage.${system}
     (inputs.zen-browser.packages.${system}.specific.overrideAttrs (prev: let
-      version = "1.0.1-a.15";
+      version = "1.0.1-a.16";
     in {
       inherit version;
       src = builtins.fetchTarball {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-        sha256 = "sha256:01rx73b8ixz7wc7vhwykfzscfpqcbfl6wk2pgmbnbb0mhmqclx6n";
+        sha256 = "sha256:0nmcr4czycgabj5zdxa7sllbnhmcn90kh55f78n9x88cda481q9f";
       };
     }))
 
