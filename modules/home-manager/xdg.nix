aba37f9f20a394fffe7533dc30cc95c8979d4e1c
Grazen0 <josedanielgrayson@proton.me>
2024-11-13 14:14:05 -0500

    refactor(xdg): add desktop portal packages (probably)

--- modules/home-manager/xdg.nix
+++ modules/home-manager/xdg.nix
@@ -1,8 +1,18 @@
-{
+{pkgs, ...}: {
   xdg = {
     userDirs = {
       enable = true;
       createDirectories = true;
+    };
+
+    portal = {
+      enable = true;
+      extraPortals = with pkgs; [
+        xdg-desktop-portal-hyprland
+        xdg-desktop-portal-gtk
+        xdg-desktop-portal-wlr
+      ];
+      config.common.default = "*";
     };
 
     mimeApps = {
