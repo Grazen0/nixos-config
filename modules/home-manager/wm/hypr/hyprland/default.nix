cfb6fe085e77e6c139c399a5d7560a28ba320c6e
Grazen0 <josedanielgrayson@proton.me>
2024-11-15 20:07:25 -0500

    refactor(hyprland): move base sutff to its default.nix

--- modules/home-manager/wm/hypr/hyprland/default.nix
+++ modules/home-manager/wm/hypr/hyprland/default.nix
@@ -1,7 +1,11 @@
 {
+  lib,
+  pkgs,
+  inputs,
+  ...
+}: {
   imports = [
     ./autostart.nix
-    ./base.nix
     ./binds.nix
     ./env.nix
     ./input.nix
@@ -11,4 +15,27 @@
     ./rules.nix
     ./workspaces.nix
   ];
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    package = inputs.hyprland.packages.${pkgs.stdenv.hostPlatform.system}.hyprland;
+
+    xwayland.enable = true;
+    systemd.enable = true;
+
+    settings = {
+      "$terminal" = "${pkgs.foot}/bin/foot";
+      "$browser" = "zen";
+      "$fileManager" = "thunar";
+
+      monitor = lib.mkDefault [", preferred, auto, 1"];
+    };
+
+    systemd.variables = ["--all"];
+  };
+
+  home.sessionVariables = {
+    NIXOS_OZONE_WL = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
+  };
 }
