55c183c65058f07a119b7006a0990cafd91abe9f
Grazen0 <josedanielgrayson@proton.me>
2024-11-23 18:19:45 -0500

    refactor: rename myOwnConfig option to grazen

--- hosts/takane/home-manager/home.nix
+++ hosts/takane/home-manager/home.nix
@@ -7,7 +7,7 @@
 
   wayland.windowManager.hyprland.settings.input.kb_layout = "latam,us";
 
-  myOwnConfig.hyprland.ewwAutoStart = ["desktop-time-alt" "desktop-fortune"];
+  grazen.hyprland.ewwAutoStart = ["desktop-time-alt" "desktop-fortune"];
 
   programs = {
     foot.settings.main.font = "${config.theme.font.regular}:size=10:weight=semibold";
