be6b3df7a9170f5a33d5fabb375a56f0025577df
Grazen0 <josedanielgrayson@proton.me>
2024-11-23 16:23:40 -0500

    refactor(bat): use kanagawa theme from vimPlugins packages

--- modules/home-manager/programs/bat.nix
+++ modules/home-manager/programs/bat.nix
@@ -6,12 +6,7 @@
     };
     themes = {
       kanagawa = {
-        src = pkgs.fetchFromGitHub {
-          owner = "rebelot";
-          repo = "kanagawa.nvim";
-          rev = "e5f7b8a804360f0a48e40d0083a97193ee4fcc87";
-          sha256 = "1066495w73a11kdpm8iah3k0pjf766ylf21jk8gjid73byl2lz0n";
-        };
+        src = pkgs.vimPlugins.kanagawa-nvim;
         file = "extras/kanagawa.tmTheme";
       };
     };
