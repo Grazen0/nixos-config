0d4715336810b445e00920ee1a9e29fda9f5e0d2
Grazen0 <josedanielgrayson@proton.me>
2024-11-28 10:44:45 -0500

    deps: update zen-browser to 1.0.1-a.22

--- overlays/default.nix
+++ overlays/default.nix
@@ -8,12 +8,12 @@
       thorium-browser = inputs.thorium-browser.defaultPackage.${system};
 
       zen-browser = inputs.zen-browser.packages.${system}.specific.overrideAttrs (prev: let
-        version = "1.0.1-a.19";
+        version = "1.0.1-a.22";
       in {
         inherit version;
         src = builtins.fetchTarball {
           url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-          sha256 = "sha256:1g7nq1yfaya97m43vnkjj1nd9g570viy8hj45c523hcyr1z92rjq";
+          sha256 = "sha256:0anr79rdm62h5y37xa47rrrk32r9gnv04df4z7brc0hp4q83pxvi";
         };
       });
     };
