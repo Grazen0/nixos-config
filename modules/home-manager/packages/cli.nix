f7f668b102e0fe8afd0ef460566fad2e53b7ccc4
Grazen0 <josedanielgrayson@proton.me>
2024-11-29 21:30:50 -0500

    feat(packages): add make-writable custom script

--- modules/home-manager/packages/cli.nix
+++ modules/home-manager/packages/cli.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [
     customScripts.exe2png
+    customScripts.make-writable
     cmatrix
     nitch
     rmtrash
--- pkgs/custom-scripts/default.nix
+++ pkgs/custom-scripts/default.nix
@@ -61,4 +61,18 @@
 
     '';
   };
+
+  make-writable = pkgs.writeShellApplication {
+    name = "make-writable";
+    text = ''
+      if [[ "$#" -lt 1 ]]; then
+        echo "No filename provided"
+        exit 1
+      fi
+
+      contents=$(cat "$1")
+      rm -f "$1"
+      cat > "$1" <<< "$contents"
+    '';
+  };
 }
