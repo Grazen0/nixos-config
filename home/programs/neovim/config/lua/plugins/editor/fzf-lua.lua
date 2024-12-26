5447d42ab3a6382b96506ccdf4e9af2ca6df9807
Grazen0 <josedanielgrayson@proton.me>
2024-12-26 16:34:52 -0500

    refactor(neovim): use fzf-lua live_grep_native instead of grep

--- home/programs/neovim/config/lua/plugins/editor/fzf-lua.lua
+++ home/programs/neovim/config/lua/plugins/editor/fzf-lua.lua
@@ -2,7 +2,7 @@ require('fzf-lua').setup()
 
 local keyset = vim.keymap.set
 keyset('n', '<leader>ff', '<cmd>FzfLua files<CR>')
-keyset('n', '<leader>fg', '<cmd>FzfLua grep<CR>')
+keyset('n', '<leader>fg', '<cmd>FzfLua live_grep_native<CR>')
 keyset('n', '<leader>fb', '<cmd>FzfLua buffers<CR>')
 keyset('n', '<leader>fh', '<cmd>FzfLua helptags<CR>')
 keyset('n', '<leader>fr', '<cmd>FzfLua lsp_references<CR>')
