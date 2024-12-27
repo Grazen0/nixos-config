b4e890dccf7280092b874ddefbf479d8841fa7a6
Grazen0 <josedanielgrayson@proton.me>
2024-12-26 19:09:39 -0500

    refactor: proper equibop settings

--- home/programs/graphical/equibop/default.nix
+++ home/programs/graphical/equibop/default.nix
@@ -1,6 +1,7 @@
 {pkgs, ...}: {
   home.packages = with pkgs; [equibop];
 
+  # TODO: maybe set settings.json from here to avoid hardcoding splash gif path
   xdg.configFile.equibop = {
     source = ./config;
     recursive = true;
