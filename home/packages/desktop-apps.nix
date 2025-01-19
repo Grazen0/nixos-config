3f11e168d32944a52dd346b24f9baecdedc51d8e
Grazen0 <josedanielgrayson@proton.me>
2025-01-19 15:01:31 -0500

    deps: update zen-browser to 1.7b

--- home/packages/desktop-apps.nix
+++ home/packages/desktop-apps.nix
@@ -9,12 +9,12 @@
     thorium-browser = inputs.thorium-browser.defaultPackage.${system};
 
     zen-browser = inputs.zen-browser.packages.${system}.specific.overrideAttrs (prev: let
-      version = "1.6b";
+      version = "1.7b";
     in {
       inherit version;
       src = builtins.fetchTarball {
         url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-x86_64.tar.bz2";
-        sha256 = "sha256:099mjcs3avw2r0b4ikp5qq35qj2farx62zhz9dw10gp6qijcz7pd";
+        sha256 = "sha256:0aig890fcynxwi7xi8jzd1w2gzg47n0jqfq76wvnkwcam611b2bp";
       };
     });
 
