c7da8e4d3a35ac6b723631a2b2b2bc6bbeca12e4
Grazen0 <josedanielgrayson@proton.me>
2025-01-27 18:23:31 -0500

    refactor(pkgs): split pkgs into subdirectories

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -10,22 +10,23 @@ in rec {
   menu-qalc = callPackage ./menu-qalc.nix {src = inputs.menu-qalc;};
 
   # Python packages
-  inkscape-figures = callPy3Package ./inkscape-figures.nix {src = inputs.inkscape-figures;};
-  mdformat-ruff = callPy3Package ./mdformat-ruff.nix {src = inputs.mdformat-ruff;};
-  mdformat-web = callPy3Package ./mdformat-web.nix {src = inputs.mdformat-web;};
-  mdformat-wikilink = callPy3Package ./mdformat-wikilink.nix {src = inputs.mdformat-wikilink;};
-  mdformat-obsidian = callPy3Package ./mdformat-obsidian.nix {
+  inkscape-figures = callPy3Package ./python/inkscape-figures.nix {src = inputs.inkscape-figures;};
+  mdformat-ruff = callPy3Package ./python/mdformat-ruff.nix {src = inputs.mdformat-ruff;};
+  mdformat-web = callPy3Package ./python/mdformat-web.nix {src = inputs.mdformat-web;};
+  mdformat-wikilink = callPy3Package ./python/mdformat-wikilink.nix {src = inputs.mdformat-wikilink;};
+  mdformat-obsidian = callPy3Package ./python/mdformat-obsidian.nix {
     inherit mdformat-ruff mdformat-web mdformat-wikilink;
     src = inputs.mdformat-obsidian;
   };
 
   # My own stuff
-  close-window = callPackage ./close-window.nix {};
-  exe2png = callPackage ./exe2png.nix {};
-  fuzzel-power-menu = callPackage ./fuzzel-power-menu.nix {};
-  handle-monitor-connect = callPackage ./handle-monitor-connect.nix {};
-  make-writable = callPackage ./make-writable.nix {};
-  volume-update = callPackage ./volume-update.nix {};
-  waybar-dunst-state = callPackage ./waybar-dunst-state.nix {};
-  waybar-media-query = callPackage ./waybar-media-query.nix {};
+  close-window = callPackage ./scripts/close-window.nix {};
+  exe2png = callPackage ./scripts/exe2png.nix {};
+  fuzzel-power-menu = callPackage ./scripts/fuzzel-power-menu.nix {};
+  handle-monitor-connect = callPackage ./scripts/handle-monitor-connect.nix {};
+  make-writable = callPackage ./scripts/make-writable.nix {};
+  volume-update = callPackage ./scripts/volume-update.nix {};
+
+  waybar-dunst-state = callPackage ./waybar/dunst-state.nix {};
+  waybar-media-query = callPackage ./waybar/media-query.nix {};
 }
