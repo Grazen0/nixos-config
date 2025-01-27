baf99600b9ab51afe517658b0887700d3edd7cb0
Grazen0 <josedanielgrayson@proton.me>
2025-01-27 11:40:48 -0500

    refactor(pkgs): e

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -2,27 +2,30 @@
   pkgs ? import <nixpkgs> {},
   inputs,
   ...
-}: rec {
+}: let
+  inherit (pkgs) callPackage;
+  callPy3Package = pkgs.python3Packages.callPackage;
+in rec {
   # Existing packages
-  menu-qalc = pkgs.callPackage ./menu-qalc.nix {src = inputs.menu-qalc;};
+  menu-qalc = callPackage ./menu-qalc.nix {src = inputs.menu-qalc;};
 
   # Python packages
-  inkscape-figures = pkgs.python3Packages.callPackage ./inkscape-figures.nix {src = inputs.inkscape-figures;};
-  mdformat-ruff = pkgs.python3Packages.callPackage ./mdformat-ruff.nix {src = inputs.mdformat-ruff;};
-  mdformat-web = pkgs.python3Packages.callPackage ./mdformat-web.nix {src = inputs.mdformat-web;};
-  mdformat-wikilink = pkgs.python3Packages.callPackage ./mdformat-wikilink.nix {src = inputs.mdformat-wikilink;};
-  mdformat-obsidian = pkgs.python3Packages.callPackage ./mdformat-obsidian.nix {
+  inkscape-figures = callPy3Package ./inkscape-figures.nix {src = inputs.inkscape-figures;};
+  mdformat-ruff = callPy3Package ./mdformat-ruff.nix {src = inputs.mdformat-ruff;};
+  mdformat-web = callPy3Package ./mdformat-web.nix {src = inputs.mdformat-web;};
+  mdformat-wikilink = callPy3Package ./mdformat-wikilink.nix {src = inputs.mdformat-wikilink;};
+  mdformat-obsidian = callPy3Package ./mdformat-obsidian.nix {
     inherit mdformat-ruff mdformat-web mdformat-wikilink;
     src = inputs.mdformat-obsidian;
   };
 
   # My own stuff
-  close-window = pkgs.callPackage ./close-window.nix {};
-  exe2png = pkgs.callPackage ./exe2png.nix {};
-  fuzzel-power-menu = pkgs.callPackage ./fuzzel-power-menu.nix {};
-  handle-monitor-connect = pkgs.callPackage ./handle-monitor-connect.nix {};
-  make-writable = pkgs.callPackage ./make-writable.nix {};
-  volume-update = pkgs.callPackage ./volume-update.nix {};
-  waybar-dunst-state = pkgs.callPackage ./waybar-dunst-state.nix {};
-  waybar-media-query = pkgs.callPackage ./waybar-media-query.nix {};
+  close-window = callPackage ./close-window.nix {};
+  exe2png = callPackage ./exe2png.nix {};
+  fuzzel-power-menu = callPackage ./fuzzel-power-menu.nix {};
+  handle-monitor-connect = callPackage ./handle-monitor-connect.nix {};
+  make-writable = callPackage ./make-writable.nix {};
+  volume-update = callPackage ./volume-update.nix {};
+  waybar-dunst-state = callPackage ./waybar-dunst-state.nix {};
+  waybar-media-query = callPackage ./waybar-media-query.nix {};
 }
