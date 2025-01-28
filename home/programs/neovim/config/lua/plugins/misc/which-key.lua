577f853cdb6c2147f64f18e7393d5ada3a148e93
Grazen0 <josedanielgrayson@proton.me>
2025-01-28 12:25:07 -0500

    refactor(neovim): which-key config changes

--- home/programs/neovim/config/lua/plugins/misc/which-key.lua
+++ home/programs/neovim/config/lua/plugins/misc/which-key.lua
@@ -1,7 +1,7 @@
 local which_key = require('which-key')
 which_key.setup({
   delay = 1000,
-  win = { wo = { winblend = 0 } },
+  win = { border = 'rounded' },
 })
 
 vim.keymap.set('n', '<leader>?', function()
