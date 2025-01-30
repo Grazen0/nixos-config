aebc26714774358e8ae449184d20ad694ef82eda
Grazen0 <josedanielgrayson@proton.me>
2025-01-30 16:30:29 -0500

    refactor(takane): disable steam

--- hosts/takane/default.nix
+++ hosts/takane/default.nix
@@ -1,9 +1,13 @@
-{lib, ...}: {
+{lib, ...}: let
+  inherit (lib) mkForce;
+in {
   imports = [
     ./hardware-configuration.nix
   ];
 
-  console.keyMap = lib.mkForce "la-latin1";
+  console.keyMap = mkForce "la-latin1";
+
+  programs.steam.enable = mkForce false;
 
   home-manager.users.jdgt.imports = [./home.nix];
 
