4ecf457f0cc726e1adcf024e7ab5f776881b3ad2
Grazen0 <josedanielgrayson@proton.me>
2025-02-02 22:01:49 -0500

    fix(neovim): quarto stuff

--- home/programs/neovim/config/lua/plugins/misc/quarto.lua
+++ home/programs/neovim/config/lua/plugins/misc/quarto.lua
@@ -1,52 +1,49 @@
-local function extra_config()
-  local runner = require('quarto.runner')
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = 'quarto',
+  callback = function()
+    require('quarto').setup({
+      codeRunner = { default_method = 'molten' },
+    })
 
-  vim.api.nvim_create_autocmd('FileType', {
-    pattern = 'quarto',
-    callback = function()
-      local keyset = vim.keymap.set
+    local runner = require('quarto.runner')
+    local keyset = vim.keymap.set
 
-      keyset(
-        'n',
-        '<C-CR>',
-        runner.run_line,
-        { desc = 'run line', silent = true, buffer = true }
-      )
-      keyset(
-        'n',
-        '<C-S-CR>',
-        runner.run_cell,
-        { desc = 'run cell', silent = true, buffer = true }
-      )
-      keyset(
-        'n',
-        '<C-A-P>',
-        runner.run_above,
-        { desc = 'run cell and above', silent = true, buffer = true }
-      )
-      keyset(
-        'v',
-        '<C-CR>',
-        runner.run_range,
-        { desc = 'run visual range', silent = true, buffer = true }
-      )
-      keyset(
-        'n',
-        '<localleader>Ra',
-        runner.run_all,
-        { desc = 'run all cells', silent = true, buffer = true }
-      )
-      keyset('n', '<localleader>RA', function()
-        runner.run_all(true)
-      end, {
-        desc = 'run all cells of all languages',
-        silent = true,
-        buffer = true,
-      })
-    end,
-  })
-end
-
-require('lib.misc').setup_on_ft('quarto', 'quarto', {
-  codeRunner = { default_method = 'molten' },
-}, extra_config)
+    keyset(
+      'n',
+      '<C-CR>',
+      runner.run_line,
+      { desc = 'run line', silent = true, buffer = true }
+    )
+    keyset(
+      'n',
+      '<C-S-CR>',
+      runner.run_cell,
+      { desc = 'run cell', silent = true, buffer = true }
+    )
+    keyset(
+      'n',
+      '<C-A-P>',
+      runner.run_above,
+      { desc = 'run cell and above', silent = true, buffer = true }
+    )
+    keyset(
+      'v',
+      '<C-CR>',
+      runner.run_range,
+      { desc = 'run visual range', silent = true, buffer = true }
+    )
+    keyset(
+      'n',
+      '<localleader>Ra',
+      runner.run_all,
+      { desc = 'run all cells', silent = true, buffer = true }
+    )
+    keyset('n', '<localleader>RA', function()
+      runner.run_all(true)
+    end, {
+      desc = 'run all cells of all languages',
+      silent = true,
+      buffer = true,
+    })
+  end,
+})
