ae4bb8b592b8e1a3ef0e07368177416e8220780e
Grazen0 <josedanielgrayson@proton.me>
2025-02-04 00:29:17 -0500

    feat(wm): swaylock settings

--- home/wm/sway/swaylock.nix
+++ home/wm/sway/swaylock.nix
@@ -1,7 +1,49 @@
 {
+  config,
+  pkgs,
+  ...
+}: {
   programs.swaylock = {
     enable = true;
-    settings = {
+    package = pkgs.swaylock-effects;
+
+    settings = let
+      inherit (config) theme;
+      colors = theme.colors.hex;
+    in {
+      font = theme.font.regular;
+
+      hide-keyboard-layout = true;
+      indicator = true;
+      indicator-radius = 100;
+      line-uses-inside = true;
+
+      clock = true;
+      timestr = "%I:%M %p";
+      datestr = "%B %-d, %Y";
+
+      color = colors.background;
+      separator-color = colors.background;
+
+      key-hl-color = colors.blue;
+      bs-hl-color = colors.red;
+
+      inside-color = colors.background;
+      inside-clear-color = colors.background;
+      inside-ver-color = colors.background;
+      inside-wrong-color = colors.background;
+
+      ring-color = colors.brightBlack;
+      ring-clear-color = colors.cyan;
+      ring-caps-lock-color = colors.magenta;
+      ring-ver-color = colors.yellow;
+      ring-wrong-color = colors.red;
+
+      text-color = colors.foreground;
+      text-clear-color = colors.cyan;
+      text-caps-lock-color = colors.magenta;
+      text-ver-color = colors.yellow;
+      text-wrong-color = colors.red;
     };
   };
 }
