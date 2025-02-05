3c478dbafe298153fea4afe8025267ff05f82088
Grazen0 <josedanielgrayson@proton.me>
2025-02-05 01:56:12 -0500

    refactor: use full neovim bin path in mainPrograms.editor

--- home/wm/main-programs.nix
+++ home/wm/main-programs.nix
@@ -7,7 +7,7 @@
     inherit (config) programs;
   in {
     terminal = "${programs.foot.package}/bin/foot";
-    editor = "nvim";
+    editor = "${programs.neovim.finalPackage}/bin/nvim";
     browser = "zen";
     fileManager = "thunar";
     fileManagerCli = "${programs.yazi.package}/bin/yazi";
