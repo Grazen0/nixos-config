88aa8eaae42de2e55158886516cb56b66a37a3cf
Grazen0 <josedanielgrayson@proton.me>
2025-02-05 17:59:12 -0500

    feat: source .config/uwsm/env from home.sessionVariables

--- home/wm/env.nix
+++ home/wm/env.nix
@@ -1,12 +1,25 @@
-{config, ...}: {
-  xdg.configFile."uwsm/env".text =
-    # bash
-    ''
-      export XDG_SESSION_TYPE="wayland"
-      export QT_GPA_PLATFORM="wayland"
-      export NIXOS_OZONE_WL="1"
-      export BEMOJI_PICKER_CMD="${config.mainPrograms.dmenu}"
-      # idc, this is for a local server
-      export OBSIDIAN_REST_API_KEY="fcaf7dede9cdedd5bbbb8e1d2889cf88974d7ed5340b1f225a01a50e8faa444e"
-    '';
+{
+  config,
+  lib,
+  ...
+}: {
+  home.sessionVariables = {
+    XDG_SESSION_TYPE = "wayland";
+    QT_GPA_PLATFORM = "wayland";
+    NIXOS_OZONE_WL = "1";
+    BEMOJI_PICKER_CMD = config.mainPrograms.dmenu;
+    # idc, this is for a local server
+    OBSIDIAN_REST_API_KEY = "fcaf7dede9cdedd5bbbb8e1d2889cf88974d7ed5340b1f225a01a50e8faa444e";
+  };
+
+  # Export home.sessionVariables from .config/uwsm/env
+  xdg.configFile."uwsm/env".text = let
+    inherit (lib) mapAttrsToList escapeShellArg concatStringsSep;
+
+    lines =
+      mapAttrsToList
+      (key: value: "export ${key}=${escapeShellArg value}")
+      config.home.sessionVariables;
+  in
+    concatStringsSep "\n" lines;
 }
