2730db5b38b727a2d9120ec3188d1f60ca8cdbaa
Grazen0 <josedanielgrayson@proton.me>
2025-02-07 10:56:56 -0500

    feat(takane): fix wlogout text height

--- hosts/takane/home.nix
+++ hosts/takane/home.nix
@@ -18,6 +18,54 @@ in {
 
     spicetify.enable = mkForce false;
     jetbrains.enable = mkForce false;
+
+    # All of this just to set the heights, bruh
+    wlogout.layout = let
+      height = 1;
+    in [
+      {
+        label = "lock";
+        action = "loginctl lock-session";
+        text = "Lock";
+        keybind = "l";
+        inherit height;
+      }
+      {
+        label = "hibernate";
+        action = "systemctl hibernate";
+        text = "Hibernate";
+        keybind = "h";
+        inherit height;
+      }
+      {
+        label = "logout";
+        action = "loginctl terminate-user $USER";
+        text = "Logout";
+        keybind = "e";
+        inherit height;
+      }
+      {
+        label = "shutdown";
+        action = "systemctl poweroff";
+        text = "Shutdown";
+        keybind = "s";
+        inherit height;
+      }
+      {
+        label = "suspend";
+        action = "systemctl suspend";
+        text = "Suspend";
+        keybind = "u";
+        inherit height;
+      }
+      {
+        label = "reboot";
+        action = "systemctl reboot";
+        text = "Reboot";
+        keybind = "r";
+        inherit height;
+      }
+    ];
   };
 
   home.stateVersion = "24.05";
