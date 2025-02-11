fac2bc86a25cfb26242cdbe8e82009c83720660a
Grazen0 <josedanielgrayson@proton.me>
2025-02-10 19:55:22 -0500

    style(fish): add comments to string for fish syntax highlighting

--- home/programs/fish/default.nix
+++ home/programs/fish/default.nix
@@ -33,64 +33,72 @@
 
       pythonEnv = {
         argumentNames = "pythonVersion";
-        body = ''
-          if set -q argv[2]
-            set argv $argv[2..-1]
-          end
+        body =
+          # fish
+          ''
+            if set -q argv[2]
+              set argv $argv[2..-1]
+            end
 
-          for el in $argv
-            set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
-          end
+            for el in $argv
+              set ppkgs $ppkgs "python"$pythonVersion"Packages.$el"
+            end
 
-          nix-shell -p $ppkgs
-        '';
+            nix-shell -p $ppkgs
+          '';
       };
 
-      nsh.body = ''
-        for pkg in $argv
-          set pkgs $pkgs "nixpkgs#$pkg"
-        end
+      nsh.body =
+        # fish
+        ''
+          for pkg in $argv
+            set pkgs $pkgs "nixpkgs#$pkg"
+          end
 
-        nix shell $pkgs
-      '';
+          nix shell $pkgs
+        '';
 
       nr = {
         argumentNames = "package";
-        body = ''
-          nix run nixpkgs#$package $argv[2..-1]
-        '';
+        body =
+          # fish
+          ''
+            nix run nixpkgs#$package $argv[2..-1]
+          '';
       };
     };
 
-    interactiveShellInit = ''
-      # Environment variables
-      set -x APHRODITE_THEME_SHOW_TIME yes
+    interactiveShellInit =
+      # fish
+      ''
+        # Environment variables
+        set -x APHRODITE_THEME_SHOW_TIME yes
 
-      source ${inputs.aphrodite-terminal-theme}/fish_prompt.fish
+        source ${inputs.aphrodite-terminal-theme}/fish_prompt.fish
 
-      # Disable greeting message
-      set -U fish_greeting
+        # Disable greeting message
+        set -U fish_greeting
 
-      # Vi cursor stuff
-      set fish_cursor_default block
-      set fish_cursor_insert line
-      set fish_cursor_replace_one underscore
-      set fish_cursor_visual block
+        # Vi cursor stuff
+        set fish_cursor_default block
+        set fish_cursor_insert line
+        set fish_cursor_replace_one underscore
+        set fish_cursor_visual block
 
-      # Colors
-      set fish_color_command green
-      set fish_color_error red
-      set fish_color_cancel black --background red
-      set fish_color_selection brwhite --background ${config.theme.colors.hex.highlight}
+        # Colors
+        set fish_color_command green
+        set fish_color_error red
+        set fish_color_cancel black --background red
+        set fish_color_selection brwhite --background ${config.theme.colors.hex.highlight}
 
-      # Bindings
-      fish_vi_key_bindings
-      for mode in normal insert
-        bind -M $mode -k nul 'accept-autosuggestion'
-        bind -M $mode \cp 'history-prefix-search-backward'
-        bind -M $mode \cn 'history-prefix-search-forward'
-        bind -M $mode \cz 'fg &> /dev/null; commandline -f repaint'
-      end
-    '';
+        # Bindings
+        fish_vi_key_bindings
+        for mode in normal insert
+          bind -M $mode -k nul 'accept-autosuggestion'
+          bind -M $mode \cp 'history-prefix-search-backward'
+          bind -M $mode \cn 'history-prefix-search-forward'
+          bind -M $mode \cz 'fg &> /dev/null; commandline -f repaint'
+        end
+      '';
   };
 }
