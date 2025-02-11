b318364d437f1886282e588bf48bc91cef8980f7
Grazen0 <josedanielgrayson@proton.me>
2025-02-10 20:51:33 -0500

    refactor(fish): remove some aliases

--- home/programs/fish/aliases.nix
+++ home/programs/fish/aliases.nix
@@ -18,17 +18,13 @@
     l = "lla -h";
     lsfr = "${pkgs.coreutils}/bin/ls";
 
-    "md" = "mkdir -p";
-    "rd" = "rmdir";
-    "rdfr" = "rmdirfr";
+    mkdir = "mkdir -p";
     ".." = "cd ..";
     "..." = "cd ../..";
     "...." = "cd ../../..";
     "....." = "cd ../../../..";
 
     grep = "grep --color=auto";
-    fgrep = "fgrep --color=auto";
-    egrep = "egrep --color=auto";
 
     g = "git";
     ga = "git add";
@@ -41,32 +37,16 @@
     gcm = "git checkout main";
     gcl = "git clone --recurse-submodules";
     gcmsg = "git commit --message";
-    gcs = "git commit --gpg-sign";
-    gc = "git commit --verbose";
     gd = "git diff";
-    gdca = "git diff --cached";
-    gdcw = "git diff --cached --word-diff";
-    gds = "git diff --staged";
-    gdw = "git diff --word-diff";
-    ghh = "git help";
     gl = "git pull";
     gp = "git push";
     gpsup = "git push --set-upstream origin $(git rev-parse --abbrev-ref HEAD)";
     grh = "git reset";
-    grhu = "git reset HEAD~"; # Undo last commit
-    grm = "git rm";
-    grmc = "git rm --cached";
     grs = "git restore";
     grst = "git restore --staged";
-    grv = "git revert";
     gstaa = "git stash apply";
-    gstc = "git stash clear";
-    gstd = "git stash drop";
-    gstl = "git stash list";
     gstp = "git stash pop";
-    gsta = "git stash push";
     gst = "git status";
-    gss = "git status --short";
 
     cat = "bat";
     catfr = "${pkgs.coreutils}/bin/cat";
@@ -95,9 +75,5 @@
     upd = "nix flake update --flake ${flakeDir}";
     nss = "nix search nixpkgs";
     ngc = "nix-collect-garbage --delete-old && sudo nix-collect-garbage --delete-old";
-
-    # I'm kinda clumsy
-    ":q" = "exit";
-    ":x" = "exit";
   };
 }
