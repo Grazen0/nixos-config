9d936f81c655906513b5011de64b741fe9630a90
Grazen0 <josedanielgrayson@proton.me>
2025-02-12 22:10:48 -0500

    refactor(pkgs): ee

--- pkgs/scripts/tmux/music.nix
+++ pkgs/scripts/tmux/music.nix
@@ -21,22 +21,15 @@ pkgs.writeShellApplication {
     fi
 
     # If anything goes wrong before attaching, kill the session
-    trap 'tmux kill-session -t "$SESSION"' EXIT
+    trap 'tmux kill-session -t $SESSION' EXIT
 
-    visualizer=$(tmux new-session -d -s "$SESSION" -x- -y- -P -F "#{pane_id}")
+    visualizer=$(tmux new-session -d -s "$SESSION" -x- -y- -P -F "#{pane_id}" "ncmpcpp -s visualizer -c '$XDG_CONFIG_HOME/ncmpcpp/visualizer'")
 
     # Open panes
-    playlist=$(tmux split-window -v -l "45%" -P -F "#{pane_id}")
-    browser=$(tmux split-window -h -l "60%" -P -F "#{pane_id}")
-    search=$(tmux split-window -h -l "50%" -P -F "#{pane_id}")
+    playlist=$(tmux split-window -v -l "45%" -P -F "#{pane_id}" "ncmpcpp -s playlist -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'" )
+    tmux split-window -h -l "60%" -P -F "#{pane_id}" "ncmpcpp -s browser -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'"
+    tmux split-window -h -l "50%" -P -F "#{pane_id}" "ncmpcpp -s search_engine -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'"
     tmux split-window -h -b -t "$visualizer" -l "30%" "player-art '$DEFAULT_IMG'"
-
-    # Open stuff
-    tmux send-keys -t "$visualizer" "ncmpcpp -s visualizer -c '$XDG_CONFIG_HOME/ncmpcpp/visualizer'" C-m
-    sleep 0.25 # The visualizer won't start otherwise for some reason
-    tmux send-keys -t "$playlist" "ncmpcpp -s playlist -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'" C-m
-    tmux send-keys -t "$browser" "ncmpcpp -s browser -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'" C-m
-    tmux send-keys -t "$search" "ncmpcpp -s search_engine -c '$XDG_CONFIG_HOME/ncmpcpp/minimal'" C-m
 
     # Focus playlist
     tmux select-pane -t "$playlist"
