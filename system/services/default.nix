b218d1871d0d09ff1fb716aff19bba913764430d
Grazen0 <josedanielgrayson@proton.me>
2025-02-16 21:23:17 -0500

    feat: add openssh config

--- system/services/default.nix
+++ system/services/default.nix
@@ -8,5 +8,19 @@
     libinput.enable = true;
     gvfs.enable = true;
     tumbler.enable = true;
+
+    openssh = {
+      enable = false;
+      ports = [22];
+      openFirewall = true;
+
+      settings = {
+        PasswordAuthentication = true;
+        AllowUsers = ["jdgt"];
+        UseDns = true;
+        X11Forwarding = false;
+        PermitRootLogin = "prohibit-password";
+      };
+    };
   };
 }
