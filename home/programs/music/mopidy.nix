d00a6fa6e39422b34569025b582d0dbc2cfbc7b2
Grazen0 <josedanielgrayson@proton.me>
2025-02-19 02:03:26 -0500

    refactor(mopidy): better sops handling

--- home/programs/music/mopidy.nix
+++ home/programs/music/mopidy.nix
@@ -1,77 +1,57 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }: {
   services.mopidy = {
     enable = true;
 
+    extraConfigFiles = [
+      config.sops.templates."mopidy-secrets.conf".path
+    ];
+
     extensionPackages = with pkgs; [
       mopidy-mpd
       mopidy-spotify
       mopidy-mpris
     ];
-  };
 
-  xdg.configFile."mopidy/mopidy.conf".enable = lib.mkForce false;
-
-  systemd.user.services = {
-    mopidy.Unit.After = ["mopidy-config.service"];
-
-    mopidy-config = {
-      Unit = {
-        Description = "Mopidy config setup";
-        After = ["sops-nix.service"];
+    settings = {
+      audio = {
+        output = "tee name=t ! queue ! autoaudiosink t. ! queue ! audio/x-raw,rate=44100,channels=2,format=S16LE ! udpsink host=localhost port=5555";
+        buffer_time = 100000;
       };
 
-      Install.WantedBy = ["default.target"];
+      file = {
+        enabled = true;
+        excluded_file_extensions = [
+          ".html"
+          ".zip"
+          ".jpg"
+          ".jpeg"
+          ".png"
+        ];
+        follow_symlinks = false;
+        media_dirs = "${config.xdg.userDirs.music}|Music";
+      };
 
-      Service.ExecStart = let
-        configFile = let
-          inherit (config.sops) secrets;
-        in
-          # toml
-          ''
-            [audio]
-            output = tee name=t ! queue ! autoaudiosink t. ! queue ! audio/x-raw,rate=44100,channels=2,format=S16LE ! udpsink host=localhost port=5555
-            buffer_time = 100000
+      mpd = {
+        enabled = true;
+        hostname = "127.0.0.1";
+        port = 6600;
+      };
 
-            [file]
-            enabled = true
-            excluded_file_extensions =
-              .html
-              .zip
-              .jpg
-              .jpeg
-              .png
-            follow_symlinks = false
-            media_dirs =
-              ${config.xdg.userDirs.music}|Music
-
-            [mpd]
-            enabled = true
-            hostname = 127.0.0.1
-            port = 6600
-
-            [spotify]
-            enabled = true
-            client_id = $(cat '${secrets."mopidy_spotify/client_id".path}')
-            client_secret = $(cat '${secrets."mopidy_spotify/client_secret".path}')
-
-          '';
-
-        pkg = pkgs.writeShellApplication {
-          name = "mopidy-config";
-          text = ''
-            mkdir -p '${config.xdg.configHome}/mopidy'
-            cat <<EOF > '${config.xdg.configHome}/mopidy/mopidy.conf'
-            ${configFile}
-            EOF
-          '';
-        };
-      in
-        lib.getExe pkg;
+      spotify.enabled = true;
     };
   };
+
+  sops.templates."mopidy-secrets.conf".content = let
+    inherit (config.sops) placeholder;
+  in
+    # toml
+    ''
+      [spotify]
+      client_id = ${placeholder."mopidy_spotify/client_id"}
+      client_secret = ${placeholder."mopidy_spotify/client_secret"}
+    '';
 }
