591db36c51f6f02d4772c8e68ae6e7165b43ff19
Grazen0 <josedanielgrayson@proton.me>
2025-02-25 21:11:01 -0500

    refactor(neovim): refactor user commands

--- home/programs/neovim/config/lua/plugins/lsp/conform.lua
+++ home/programs/neovim/config/lua/plugins/lsp/conform.lua
@@ -1,3 +1,5 @@
+local create_user_command = vim.api.nvim_create_user_command
+
 return {
   'stevearc/conform.nvim',
   cmd = 'Format',
@@ -45,4 +47,54 @@ return {
       mdformat = { append_args = { '--number' } },
     },
   },
+
+  init = function()
+    -- Toggle formatting
+    create_user_command('FormatEnable', function()
+      vim.b.disable_autoformat = false
+      vim.g.disable_autoformat = false
+    end, { bang = true })
+
+    create_user_command('FormatDisable', function(args)
+      if args.bang then
+        -- FormatDisable! will disable formatting just for this buffer
+        vim.b.disable_autoformat = true
+      else
+        vim.g.disable_autoformat = true
+      end
+    end, { bang = true })
+
+    -- Save without formatting
+    create_user_command('Mfw', function()
+      local prev = vim.b.disable_autoformat
+      vim.b.disable_autoformat = true
+      vim.cmd('write')
+      vim.b.disable_autoformat = prev
+    end, {})
+  end,
+
+  config = function(_, opts)
+    local conform = require('conform')
+    conform.setup(opts)
+
+    -- Format command
+    create_user_command('Format', function(args)
+      local range = nil
+
+      if args.count ~= -1 then
+        local end_line =
+          vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
+        range = {
+          start = { args.line1, 0 },
+          ['end'] = { args.line2, end_line:len() },
+        }
+      end
+
+      conform.format({
+        async = true,
+        lsp_format = 'fallback',
+        range = range,
+      })
+    end, { range = true })
+  end,
 }
--- home/programs/neovim/config/lua/user-commands.lua
+++ home/programs/neovim/config/lua/user-commands.lua
@@ -1,56 +1,19 @@
 local create_user_command = vim.api.nvim_create_user_command
 
+local function alias_with_bang(cmd)
+  return function(args)
+    if args.bang then
+      vim.cmd(cmd .. '!')
+    else
+      vim.cmd(cmd)
+    end
+  end
+end
+
 -- I'm kinda clumsy
-create_user_command('W', 'w', {})
-create_user_command('Wa', 'wa', {})
-create_user_command('X', 'x', {})
-create_user_command('Xa', 'xa', {})
-create_user_command('Q', 'q', {})
-create_user_command('Qa', 'qa', {})
-
--- Format bufffer
-local conform = require('conform')
-
-create_user_command('Format', function(args)
-  local range = nil
-
-  if args.count ~= -1 then
-    local end_line =
-      vim.api.nvim_buf_get_lines(0, args.line2 - 1, args.line2, true)[1]
-    range = {
-      start = { args.line1, 0 },
-      ['end'] = { args.line2, end_line:len() },
-    }
-  end
-
-  conform.format({
-    async = true,
-    lsp_format = 'fallback',
-    range = range,
-  })
-end, { range = true })
-
--- Toggle formatting
-create_user_command('FormatEnable', function()
-  vim.b.disable_autoformat = false
-  vim.g.disable_autoformat = false
-end, { bang = true })
-
-create_user_command('FormatDisable', function(args)
-  if args.bang then
-    -- FormatDisable! will disable formatting just for this buffer
-    vim.b.disable_autoformat = true
-  else
-    vim.g.disable_autoformat = true
-  end
-end, { bang = true })
-
--- Save without formatting
-create_user_command('Mfw', function()
-  local prev = vim.b.disable_autoformat
-  vim.b.disable_autoformat = true
-
-  vim.cmd('write')
-
-  vim.b.disable_autoformat = prev
-end, {})
+create_user_command('W', alias_with_bang('w'), { bang = true })
+create_user_command('Wa', alias_with_bang('wa'), { bang = true })
+create_user_command('X', alias_with_bang('x'), { bang = true })
+create_user_command('Xa', alias_with_bang('xa'), { bang = true })
+create_user_command('Q', alias_with_bang('q'), { bang = true })
+create_user_command('Qa', alias_with_bang('qa'), { bang = true })
