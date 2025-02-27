66ab0207b1360928d5be7f0a572dce643c982409
Grazen0 <josedanielgrayson@proton.me>
2025-02-26 23:37:40 -0500

    refactor(neovim): change vimtex latexmk output dir

--- home/programs/neovim/config/lua/plugins/misc/init.lua
+++ home/programs/neovim/config/lua/plugins/misc/init.lua
@@ -58,7 +58,7 @@ return {
     init = function()
       vim.g.vimtex_view_method = 'zathura'
       vim.g.vimtex_compiler_latexmk = {
-        out_dir = 'dist',
+        out_dir = 'build',
       }
     end,
   },
