934e444a7566624721505985bbb5b3217d4713e5
Grazen0 <josedanielgrayson@proton.me>
2025-02-27 00:43:51 -0500

    fix(neovim): fix nvim-web-devicons overrides not working

--- home/programs/neovim/config/lua/plugins/editor/spectre.lua
+++ home/programs/neovim/config/lua/plugins/editor/spectre.lua
@@ -1,17 +1,18 @@
-local spectre = require('spectre')
-
 return {
   'nvim-pack/nvim-spectre',
   cmd = 'Spectre',
-  config = function(_, opts)
-    require('spectre').setup(opts)
-  end,
   keys = {
-    { '<leader>S', spectre.toggle, desc = 'Toggle Spectre' },
+    {
+      '<leader>S',
+      function()
+        require('spectre').toggle()
+      end,
+      desc = 'Toggle Spectre',
+    },
     {
       '<leader>sw',
       function()
-        spectre.open_visual({ select_word = true })
+        require('spectre').open_visual({ select_word = true })
       end,
       { desc = 'Search current word' },
     },
@@ -19,16 +20,17 @@ return {
       '<leader>sw',
       function()
         vim.api.nvim_feedkeys('', 't', true)
-        spectre.open_visual()
+        require('spectre').open_visual()
       end,
       { desc = 'Search current word' },
     },
     {
       '<leader>sp',
       function()
-        spectre.open_file_search({ select_word = true })
+        require('spectre').open_file_search({ select_word = true })
       end,
       { desc = 'Search on current file' },
     },
   },
+  opts = {},
 }
--- home/programs/neovim/config/lua/plugins/visuals/web-devicons.lua
+++ home/programs/neovim/config/lua/plugins/visuals/web-devicons.lua
@@ -1,30 +1,30 @@
 return {
   'nvim-tree/nvim-web-devicons',
   lazy = false,
+  priority = 1000,
   opts = {
-    -- FIX: does not work
     override_by_extension = {
-      asm = {
+      ['asm'] = {
+        icon = '',
+        color = '#ff9e3b',
+        name = 'Asm',
+      },
+      ['s'] = {
         icon = '',
         color = '#ff9e3b',
         name = 'Asm',
       },
-      s = {
-        icon = '',
-        color = '#ff9e3b',
-        name = 'Asm',
-      },
-      inc = {
+      ['inc'] = {
         icon = '',
         color = '#969696',
         name = 'Inc',
       },
-      envrc = {
+      ['envrc'] = {
         icon = '',
         color = '#c8c093',
         name = 'Envrc',
       },
-      qmd = {
+      ['qmd'] = {
         icon = '',
         color = '#7e9cd8',
         name = 'Quarto',
