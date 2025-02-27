de244de20121b230adacb6ce3ffb8fa41931b15b
Grazen0 <josedanielgrayson@proton.me>
2025-02-27 15:42:07 -0500

    feat(neovim): expand snippets with <Tab>

--- home/programs/neovim/config/lua/plugins/editor/blink-cmp.lua
+++ home/programs/neovim/config/lua/plugins/editor/blink-cmp.lua
@@ -8,72 +8,91 @@ return {
     'saghen/blink.cmp',
     event = { 'BufReadPost', 'BufNewFile' },
     dependencies = { 'L3MON4D3/luasnip' },
-    opts = {
-      keymap = { preset = 'enter' },
-      cmdline = {
+    opts = function()
+      local blink = require('blink.cmp')
+
+      return {
         keymap = {
           preset = 'enter',
-          ['<Tab>'] = { 'show', 'select_next', 'fallback' },
-          ['<S-Tab>'] = { 'select_prev', 'fallback' },
-        },
-      },
-      snippets = { preset = 'luasnip' },
-      sources = {
-        default = {
-          'lsp',
-          'path',
-          'snippets',
-          'buffer',
-          'lazydev',
-          'obsidian',
-          'obsidian_new',
-          'obsidian_tags',
-        },
-        providers = {
-          lazydev = {
-            name = 'LazyDev',
-            module = 'lazydev.integrations.blink',
-            score_offset = 100,
-          },
-          obsidian = {
-            name = 'obsidian',
-            module = 'blink.compat.source',
-          },
-          obsidian_new = {
-            name = 'obsidian_new',
-            module = 'blink.compat.source',
-          },
-          obsidian_tags = {
-            name = 'obsidian_tags',
-            module = 'blink.compat.source',
+          ['<Tab>'] = {
+            function()
+              local ls = require('luasnip')
+
+              if ls.expandable() then
+                blink.hide()
+                vim.schedule(ls.expand)
+                return true
+              end
+            end,
+            'snippet_forward',
+            'fallback',
           },
         },
-      },
-      signature = { enabled = true },
-      completion = {
-        ghost_text = { enabled = true },
-        documentation = {
-          auto_show = true,
-          auto_show_delay_ms = 500,
-        },
-        list = {
-          selection = {
-            preselect = false,
-            auto_insert = false,
+        cmdline = {
+          keymap = {
+            preset = 'enter',
+            ['<Tab>'] = { 'show', 'select_next', 'fallback' },
+            ['<S-Tab>'] = { 'select_prev', 'fallback' },
           },
         },
-        menu = {
-          draw = {
-            columns = {
-              { 'label', 'label_description', gap = 1 },
-              { 'kind_icon', 'kind', gap = 1 },
+        snippets = { preset = 'luasnip' },
+        sources = {
+          default = {
+            'lsp',
+            'path',
+            'snippets',
+            'buffer',
+            'lazydev',
+            'obsidian',
+            'obsidian_new',
+            'obsidian_tags',
+          },
+          providers = {
+            lazydev = {
+              name = 'LazyDev',
+              module = 'lazydev.integrations.blink',
+              score_offset = 100,
+            },
+            obsidian = {
+              name = 'obsidian',
+              module = 'blink.compat.source',
+            },
+            obsidian_new = {
+              name = 'obsidian_new',
+              module = 'blink.compat.source',
+            },
+            obsidian_tags = {
+              name = 'obsidian_tags',
+              module = 'blink.compat.source',
             },
           },
-          auto_show = function(ctx)
-            return ctx.mode ~= 'cmdline'
-          end,
         },
-      },
-    },
+        signature = { enabled = true },
+        completion = {
+          ghost_text = { enabled = true },
+          documentation = {
+            auto_show = true,
+            auto_show_delay_ms = 500,
+          },
+          list = {
+            selection = {
+              preselect = false,
+              auto_insert = false,
+            },
+          },
+          menu = {
+            draw = {
+              columns = {
+                { 'label', 'label_description', gap = 1 },
+                { 'kind_icon', 'kind', gap = 1 },
+              },
+            },
+            auto_show = function(ctx)
+              return ctx.mode ~= 'cmdline'
+            end,
+          },
+        },
+      }
+    end,
   },
 }
