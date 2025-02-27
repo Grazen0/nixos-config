cac8a706ea808968625cb25aafc44773d5c19497
Grazen0 <josedanielgrayson@proton.me>
2025-02-27 16:06:22 -0500

    perf(neovim): lazy-load typescript-tools.nvim

--- home/programs/neovim/config/lua/plugins/lsp/init.lua
+++ home/programs/neovim/config/lua/plugins/lsp/init.lua
@@ -44,6 +44,14 @@ return {
   },
   {
     'pmizio/typescript-tools.nvim',
+    ft = {
+      'javascript',
+      'javascriptreact',
+      'javascript.jsx',
+      'typescript',
+      'typescriptreact',
+      'typescript.tsx',
+    },
     dependencies = { 'nvim-lua/plenary.nvim', 'neovim/nvim-lspconfig' },
     opts = {},
   },
