3d5c6ffc510d323c01cb2492cda83b1a574b5eb8
Grazen0 <josedanielgrayson@proton.me>
2025-03-01 22:42:20 -0500

    refactor(neovim): disable gitsigns current line blame

--- home/programs/neovim/config/lua/plugins/visuals/init.lua
+++ home/programs/neovim/config/lua/plugins/visuals/init.lua
@@ -52,11 +52,6 @@ return {
   {
     'lewis6991/gitsigns.nvim',
     event = { 'BufReadPre', 'BufNewFile' },
-    opts = {
-      current_line_blame = true,
-      current_line_blame_opts = {
-        delay = 500,
-      },
-    },
+    opts = {},
   },
 }
