f1993a9adf751315b84e18d93e69c89e9c73142f
Grazen0 <josedanielgrayson@proton.me>
2025-03-09 01:20:42 -0500

    refactor(neovim): make multicursor.nvim lazy-load on keys + remove spectre keys

--- home/programs/neovim/config/lua/plugins/editor/multicursor.lua
+++ home/programs/neovim/config/lua/plugins/editor/multicursor.lua
@@ -1,113 +1,168 @@
 return {
   'jake-stewart/multicursor.nvim',
-  event = { 'BufReadPre', 'BufNewFile' },
-  config = function(_, opts)
-    local mc = require('multicursor-nvim')
-    mc.setup(opts)
-
-    local set = vim.keymap.set
-
+  lazy = true,
+  keys = {
     -- Add or skip cursor above/below the main cursor.
-    set({ 'n', 'x' }, '<up>', function()
-      mc.lineAddCursor(-1)
-    end)
-    set({ 'n', 'x' }, '<down>', function()
-      mc.lineAddCursor(1)
-    end)
-    set({ 'n', 'x' }, '<leader><up>', function()
-      mc.lineSkipCursor(-1)
-    end)
-    set({ 'n', 'x' }, '<leader><down>', function()
-      mc.lineSkipCursor(1)
-    end)
+    {
+      mode = { 'n', 'x' },
+      '<Up>',
+      function()
+        require('multicursor-nvim').lineAddCursor(-1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<Down>',
+      function()
+        require('multicursor-nvim').lineAddCursor(1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<leader><Up>',
+      function()
+        require('multicursor-nvim').lineSkipCursor(-1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<leader><Down>',
+      function()
+        require('multicursor-nvim').lineSkipCursor(1)
+      end,
+    },
 
     -- Add or skip adding a new cursor by matching word/selection
-    -- set({ 'n', 'x' }, '<leader>n', function()
-    --   mc.matchAddCursor(1)
-    -- end)
-    -- set({ 'n', 'x' }, '<leader>s', function()
-    --   mc.matchSkipCursor(1)
-    -- end)
-    -- set({ 'n', 'x' }, '<leader>N', function()
-    --   mc.matchAddCursor(-1)
-    -- end)
-    -- set({ 'n', 'x' }, '<leader>S', function()
-    --   mc.matchSkipCursor(-1)
-    -- end)
+    {
+      mode = { 'n', 'x' },
+      '<leader>n',
+      function()
+        require('multicursor-nvim').matchAddCursor(1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<leader>s',
+      function()
+        require('multicursor-nvim').matchSkipCursor(1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<leader>N',
+      function()
+        require('multicursor-nvim').matchAddCursor(-1)
+      end,
+    },
+    {
+      mode = { 'n', 'x' },
+      '<leader>S',
+      function()
+        require('multicursor-nvim').matchSkipCursor(-1)
+      end,
+    },
 
     -- In normal/visual mode, press `mwap` will create a cursor in every match of
     -- the word captured by `iw` (or visually selected range) inside the bigger
     -- range specified by `ap`. Useful to replace a word inside a function, e.g. mwif.
-    set({ 'n', 'x' }, 'mw', function()
-      mc.operator({ motion = 'iw', visual = true })
-      -- Or you can pass a pattern, press `mwi{` will select every \w,
-      -- basically every char in a `{ a, b, c, d }`.
-      -- mc.operator({ pattern = [[\<\w]] })
-    end)
+    {
+      mode = { 'n', 'x' },
+      'mw',
+      function()
+        require('multicursor-nvim').operator({ motion = 'iw', visual = true })
+      end,
+    },
 
     -- Press `mWi"ap` will create a cursor in every match of string captured by `i"` inside range `ap`.
-    set('n', 'mW', mc.operator)
+    { mode = 'n', 'mW', require('multicursor-nvim').operator },
 
     -- Add all matches in the document
-    set({ 'n', 'x' }, '<leader>A', mc.matchAllAddCursors)
+    {
+      mode = { 'n', 'x' },
+      '<leader>A',
+      require('multicursor-nvim').matchAllAddCursors,
+    },
 
     -- Rotate the main cursor.
-    set({ 'n', 'x' }, '<left>', mc.nextCursor)
-    set({ 'n', 'x' }, '<right>', mc.prevCursor)
+    { mode = { 'n', 'x' }, '<left>', require('multicursor-nvim').nextCursor },
+    { mode = { 'n', 'x' }, '<right>', require('multicursor-nvim').prevCursor },
 
     -- Delete the main cursor.
-    -- set({ 'n', 'x' }, '<leader>x', mc.deleteCursor)
+    -- set({ 'n', 'x' }, '<leader>x', require('multicursor-nvim').deleteCursor)
 
     -- Add and remove cursors with control + left click.
-    set('n', '<c-leftmouse>', mc.handleMouse)
+    { mode = 'n', '<c-leftmouse>', require('multicursor-nvim').handleMouse },
 
-    set('n', '<c-leftdrag>', mc.handleMouseDrag)
-    set('n', '<c-leftrelease>', mc.handleMouseRelease)
+    { mode = 'n', '<c-leftdrag>', require('multicursor-nvim').handleMouseDrag },
+    {
+      mode = 'n',
+      '<c-leftrelease>',
+      require('multicursor-nvim').handleMouseRelease,
+    },
 
     -- Easy way to add and remove cursors using the main cursor.
-    set({ 'n', 'x' }, '<c-q>', mc.toggleCursor)
+    { mode = { 'n', 'x' }, '<c-q>', require('multicursor-nvim').toggleCursor },
 
     -- Clone every cursor and disable the originals.
-    set({ 'n', 'x' }, '<leader><c-q>', mc.duplicateCursors)
+    {
+      mode = { 'n', 'x' },
+      '<leader><c-q>',
+      require('multicursor-nvim').duplicateCursors,
+    },
 
-    set('n', '<Esc>', function()
-      if not mc.cursorsEnabled() then
-        mc.enableCursors()
-      elseif mc.hasCursors() then
-        mc.clearCursors()
-      else
-        -- Default <esc> handler.
-        vim.cmd('nohl')
-      end
-    end)
+    {
+      mode = 'n',
+      '<Esc>',
+      function()
+        if not require('multicursor-nvim').cursorsEnabled() then
+          require('multicursor-nvim').enableCursors()
+        elseif require('multicursor-nvim').hasCursors() then
+          require('multicursor-nvim').clearCursors()
+        else
+          -- Default <esc> handler.
+          vim.cmd('nohl')
+        end
+      end,
+    },
 
     -- bring back cursors if you accidentally clear them
-    set('n', '<leader>gv', mc.restoreCursors)
+    { mode = 'n', '<leader>gv', require('multicursor-nvim').restoreCursors },
 
     -- Align cursor columns.
-    set('n', '<leader>a', mc.alignCursors)
+    { mode = 'n', '<leader>a', require('multicursor-nvim').alignCursors },
 
     -- Split visual selections by regex.
-    -- set('x', 'S', mc.splitCursors)
+    -- set('x', 'S', require('multicursor-nvim').splitCursors)
 
     -- Append/insert for each line of visual selections.
-    set('x', 'I', mc.insertVisual)
-    set('x', 'A', mc.appendVisual)
+    { mode = 'x', 'I', require('multicursor-nvim').insertVisual },
+    { mode = 'x', 'A', require('multicursor-nvim').appendVisual },
 
     -- match new cursors within visual selections by regex.
-    set('x', 'M', mc.matchCursors)
+    { mode = 'x', 'M', require('multicursor-nvim').matchCursors },
 
     -- Rotate visual selection contents.
-    set('x', '<leader>t', function()
-      mc.transposeCursors(1)
-    end)
-    set('x', '<leader>T', function()
-      mc.transposeCursors(-1)
-    end)
+    {
+      mode = 'x',
+      '<leader>t',
+      function()
+        require('multicursor-nvim').transposeCursors(1)
+      end,
+    },
+    {
+      mode = 'x',
+      '<leader>T',
+      function()
+        require('multicursor-nvim').transposeCursors(-1)
+      end,
+    },
 
     -- Jumplist support
-    set({ 'x', 'n' }, '<c-i>', mc.jumpForward)
-    set({ 'x', 'n' }, '<c-o>', mc.jumpBackward)
+    { mode = { 'x', 'n' }, '<C-i>', require('multicursor-nvim').jumpForward },
+    { mode = { 'x', 'n' }, '<C-o>', require('multicursor-nvim').jumpBackward },
+  },
+  config = function(_, opts)
+    require('multicursor-nvim').setup(opts)
 
     -- Customize how cursors look.
     local hl = vim.api.nvim_set_hl
--- home/programs/neovim/config/lua/plugins/editor/spectre.lua
+++ home/programs/neovim/config/lua/plugins/editor/spectre.lua
@@ -1,36 +1,5 @@
 return {
   'nvim-pack/nvim-spectre',
   cmd = 'Spectre',
-  keys = {
-    {
-      '<leader>S',
-      function()
-        require('spectre').toggle()
-      end,
-      desc = 'Toggle Spectre',
-    },
-    {
-      '<leader>sw',
-      function()
-        require('spectre').open_visual({ select_word = true })
-      end,
-      { desc = 'Search current word' },
-    },
-    {
-      '<leader>sw',
-      function()
-        vim.api.nvim_feedkeys('', 't', true)
-        require('spectre').open_visual()
-      end,
-      { desc = 'Search current word' },
-    },
-    {
-      '<leader>sp',
-      function()
-        require('spectre').open_file_search({ select_word = true })
-      end,
-      { desc = 'Search on current file' },
-    },
-  },
   opts = {},
 }
