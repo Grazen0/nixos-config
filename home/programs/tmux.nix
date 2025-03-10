9075601ee993f2e63f91e65f57860cef8f2709d2
Grazen0 <josedanielgrayson@proton.me>
2025-03-09 19:55:38 -0500

    fix(tmux): make undercurls and italics work
    maybe bolds too

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -82,6 +82,7 @@
         bind -r C-l resize-pane -R
 
         # https://reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+        set -g default-terminal "tmux-256color"
         set -ga terminal-overrides ",*256col*:Tc"
         set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
         set-environment -g COLORTERM "truecolor"
