7e0048fb1d0ac98f2e1cf6c8a4a2f35336b26bbb
Grazen0 <josedanielgrayson@proton.me>
2025-03-13 02:43:09 -0500

    fix(pkgs): reduce tmux session picker height by 1

--- pkgs/scripts/tmux/session-picker.nix
+++ pkgs/scripts/tmux/session-picker.nix
@@ -11,7 +11,7 @@ writeShellApplication {
     sessions=$(tmux list-sessions -F '#S')
     sessions_len=$(echo "$sessions" | wc -l)
 
-    tmux_flags="center,24,$((sessions_len + 4)),border-native"
+    tmux_flags="center,24,$((sessions_len + 3)),border-native"
 
     if selection=$(echo "$sessions" | fzf --tmux="$tmux_flags" --no-separator --info=hidden --layout=reverse); then
         tmux switch-client -t "$selection"
