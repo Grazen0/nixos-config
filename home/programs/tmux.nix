d139e18fdbfcb84bb813e4aca3c6607c9fedfe22
Grazen0 <josedanielgrayson@proton.me>
2025-03-16 22:41:24 -0500

    fix(tmux): fix undercurl/italics not working
    Fixed by setting default-terminal from the nix option instead of from
    extraConfig

--- home/programs/tmux.nix
+++ home/programs/tmux.nix
@@ -61,6 +61,7 @@
         }
       ];
 
+    terminal = "tmux-256color";
     shortcut = "a";
     baseIndex = 1;
     mouse = true;
@@ -85,7 +86,6 @@
         bind -r C-l resize-pane -R
 
         # https://reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-        set -g default-terminal "tmux-256color"
         set -ga terminal-overrides ",*256col*:Tc"
         set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
         set-environment -g COLORTERM "truecolor"
