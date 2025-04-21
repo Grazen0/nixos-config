903224e4fc61df89ba262ed23dfde700bb405f11
José Daniel Grayson <josedanielgrayson@proton.me>
2025-04-20 20:21:52 -0500

    build: revert back to non-shallow nixpkgs

--- flake.nix
+++ flake.nix
@@ -38,8 +38,9 @@
     };
 
   inputs = {
-    nixpkgs.url = "git+https://github.com/NixOS/nixpkgs?shallow=1&ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-24.11";
+
     nixpkgs-zoom.url = "github:NixOS/nixpkgs/bcb68885668cccec12276bbb379f8f2557aa06ce";
 
     treefmt-nix = {
