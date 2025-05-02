689079e5e9d49aedf504ac2c3e6c9d9b98694e24
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-02 08:34:36 -0500

    refactor(neovim): remove some unneeded treesitter binds

--- profiles/home/ttymax/programs/neovim/config/lua/plugins/lsp/treesitter.lua
+++ profiles/home/ttymax/programs/neovim/config/lua/plugins/lsp/treesitter.lua
@@ -27,9 +27,6 @@ return {
       navigation = {
         enable = true,
         keymaps = {
-          goto_definition_lsp_fallback = 'gd',
-          list_definitions = 'gD',
-          list_definitions_toc = 'gO',
           goto_next_usage = '<A-*>',
           goto_previous_usage = '<A-#>',
         },
