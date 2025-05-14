e9393c2d08aaa6797cb1a54567de8a6d3bd5eb19
José Daniel Grayson <josedanielgrayson@proton.me>
2025-05-14 10:30:59 -0500

    refactor(boot): disable plymouth

--- profiles/nixos/graphical/default.nix
+++ profiles/nixos/graphical/default.nix
@@ -66,20 +66,20 @@
   ];
 
   boot = {
-    plymouth.enable = true;
+    # plymouth.enable = true;
 
     # Silent boot
-    consoleLogLevel = 0;
-    initrd.verbose = false;
-    kernelParams = [
-      "quiet"
-      "splash"
-      "boot.shell_on_fail"
-      "loglevel=3"
-      "rd.systemd.show_status=false"
-      "rd.udev.log_level=3"
-      "udev.log_priority=3"
-    ];
+    # consoleLogLevel = 0;
+    # initrd.verbose = false;
+    # kernelParams = [
+    #   "quiet"
+    #   "splash"
+    #   "boot.shell_on_fail"
+    #   "loglevel=3"
+    #   "rd.systemd.show_status=false"
+    #   "rd.udev.log_level=3"
+    #   "udev.log_priority=3"
+    # ];
 
     # v4l2loopback module required for OBS Virtual Camera
     extraModulePackages = with config.boot.kernelPackages; [ v4l2loopback ];
