7b32652b461f70cb08105174208bb5e18c668f85
José Daniel Grayson <josedanielgrayson@proton.me>
2025-06-19 22:24:44 -0500

    feat(neovim): enable denols

--- profiles/home/ttymax/programs/neovim/config/lua/plugins/lsp/lspconfig.lua
+++ profiles/home/ttymax/programs/neovim/config/lua/plugins/lsp/lspconfig.lua
@@ -3,83 +3,93 @@ return {
   event = { 'BufReadPost', 'BufNewFile' },
   cmd = { 'LspInfo', 'LspInstall', 'LspUninstall' },
   dependencies = { 'saghen/blink.cmp' },
-  opts = {
-    servers = {
-      bashls = {},
-      clangd = {
-        cmd = {
-          'clangd',
-          '--background-index',
-          '--clang-tidy',
-          '--completion-style=detailed',
-          '--header-insertion=iwyu',
-          '--pch-storage=memory',
+  opts = function()
+    local util = require('lspconfig.util')
+
+    return {
+      servers = {
+        bashls = {},
+        clangd = {
+          cmd = {
+            'clangd',
+            '--background-index',
+            '--clang-tidy',
+            '--completion-style=detailed',
+            '--header-insertion=iwyu',
+            '--pch-storage=memory',
+          },
         },
-      },
-      nil_ls = {},
-      lua_ls = {},
-      vtsls = {},
-      eslint = {},
-      html = {},
-      -- emmet_language_server = {},
-      cssls = {},
-      svelte = {},
-      pyright = {},
-      texlab = {},
-      gopls = {},
-      rust_analyzer = {
-        settings = {
-          ['rust-analyzer'] = {
-            check = {
-              command = 'clippy',
+        nil_ls = {},
+        lua_ls = {},
+        vtsls = {
+          root_dir = util.root_pattern('package.json'),
+          single_file_support = false,
+        },
+        denols = {
+          root_dir = util.root_pattern('deno.json', 'deno.jsonc'),
+        },
+        eslint = {},
+        html = {},
+        -- emmet_language_server = {},
+        cssls = {},
+        svelte = {},
+        pyright = {},
+        texlab = {},
+        gopls = {},
+        rust_analyzer = {
+          settings = {
+            ['rust-analyzer'] = {
+              check = {
+                command = 'clippy',
+              },
             },
           },
         },
+        -- r_language_server = {},
+        -- zls = {},
+        cmake = {},
+        autotools_ls = {},
+        taplo = {},
+        vimls = {},
+        jsonls = {
+          settings = {
+            json = {
+              schemas = require('schemastore').json.schemas(),
+              validate = { enable = true },
+            },
+          },
+        },
+        yamlls = {
+          settings = {
+            yaml = {
+              schemas = require('schemastore').yaml.schemas(),
+              schemaStore = { enable = false, url = '' },
+            },
+          },
+        },
+        -- arduino_language_server = {},
       },
-      -- r_language_server = {},
-      -- zls = {},
-      cmake = {},
-      autotools_ls = {},
-      taplo = {},
-      vimls = {},
-      jsonls = {
-        settings = {
-          json = {
-            schemas = require('schemastore').json.schemas(),
-            validate = { enable = true },
+      diagnostic_config = {
+        virtual_text = true,
+        signs = {
+          text = {
+            [vim.diagnostic.severity.ERROR] = ' ',
+            [vim.diagnostic.severity.WARN] = ' ',
+            [vim.diagnostic.severity.HINT] = ' ',
+            [vim.diagnostic.severity.INFO] = ' ',
           },
         },
       },
-      yamlls = {
-        settings = {
-          yaml = {
-            schemas = require('schemastore').yaml.schemas(),
-            schemaStore = { enable = false, url = '' },
+      default_capabilities = {
+        textDocument = {
+          foldingRange = {
+            dynamicRegistration = false,
+            lineFoldingOnly = true,
           },
         },
       },
-      -- arduino_language_server = {},
-    },
-    diagnostic_config = {
-      virtual_text = true,
-      signs = {
-        text = {
-          [vim.diagnostic.severity.ERROR] = ' ',
-          [vim.diagnostic.severity.WARN] = ' ',
-          [vim.diagnostic.severity.HINT] = ' ',
-          [vim.diagnostic.severity.INFO] = ' ',
-        },
-      },
-    },
-    default_capabilities = {
-      textDocument = {
-        foldingRange = {
-          dynamicRegistration = false,
-          lineFoldingOnly = true,
-        },
-      },
-    },
-  },
+    }
+  end,
   config = function(_, opts)
     vim.diagnostic.config(opts.diagnostic_config)
 
