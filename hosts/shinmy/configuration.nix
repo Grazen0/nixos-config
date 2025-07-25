2d8aed8a87fe0b285bc59bd7e67f7dbbb9c5e84b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-07-25 15:13:48 -0500

    feat(shinmy): enable openssh

--- hosts/shinmy/configuration.nix
+++ hosts/shinmy/configuration.nix
@@ -3,6 +3,18 @@
     ./hardware-configuration.nix
   ];
 
+  programs.neovim.enable = true;
+
+  services.openssh = {
+    enable = true;
+    settings = {
+      PasswordAuthentication = true;
+      KbdInteractiveAuthentication = true;
+      PermitRootLogin = "no";
+      AllowUsers = [ "jdgt" ];
+    };
+  };
+
   networking.hostName = "shinmy";
   system.stateVersion = "24.05";
 }
