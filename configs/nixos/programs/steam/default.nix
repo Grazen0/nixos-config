1c77a4aec20abee26b18aac37097e81a2310d83e
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-12 15:51:24 -0500

    fix: remove enable = true from steam config

--- configs/nixos/programs/steam/default.nix
+++ configs/nixos/programs/steam/default.nix
@@ -9,7 +9,6 @@
   environment.systemPackages = lib.optionals config.programs.steam.enable (with pkgs; [ steam-run ]);
 
   programs.steam = {
-    enable = true;
     remotePlay.openFirewall = true;
     dedicatedServer.openFirewall = true;
   };
