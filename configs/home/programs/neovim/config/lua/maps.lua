3d0e634c73121b66de71e3cf70d9518dc74b3277
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-16 01:58:16 -0500

    feat(neovim): refactor maps file and add unnamedplus maps

--- configs/home/programs/neovim/config/lua/maps.lua
+++ configs/home/programs/neovim/config/lua/maps.lua
@@ -5,26 +5,29 @@ local map = vim.keymap.set
 
 map('n', '<esc>', '<cmd>nohlsearch<cr>')
 
--- gh/gl instead of ^/$
-map({ 'n', 'v' }, 'gh', '^')
-map({ 'n', 'v' }, 'gl', '$')
+-- Helix-like bindings
+map({ 'n', 'v', 'x' }, 'U', '<c-r>')
+map({ 'n', 'v', 'x' }, 'gh', '^')
+map({ 'n', 'v', 'x' }, 'gl', '$')
+map({ 'n', 'v', 'x' }, 'gH', 'g^')
+map({ 'n', 'v', 'x' }, 'gL', 'g$')
 
--- Better redo
-map('n', 'U', '<c-r>')
+map({ 'n', 'v', 'x' }, '<leader>y', '"+y')
+map({ 'n', 'v', 'x' }, '<leader>d', '"+d')
+map({ 'n', 'v', 'x' }, '<leader>p', '"+p')
+map('n', '<esc>', '<cmd>nohlsearch<cr>')
+map('n', '<leader>s', '<cmd>e #<cr>')
+map('n', '<leader>d', vim.diagnostic.open_float)
 
 -- Window navigation
 map('n', '<c-h>', '<c-w>h')
 map('n', '<c-j>', '<c-w>j')
 map('n', '<c-k>', '<c-w>k')
 map('n', '<c-l>', '<c-w>l')
-
--- Window positioning
 map('n', '<a-h>', '<c-w>H')
 map('n', '<a-j>', '<c-w>J')
 map('n', '<a-k>', '<c-w>K')
 map('n', '<a-l>', '<c-w>L')
-
--- Window resizing
 map('n', '<a-,>', '<c-w><lt>')
 map('n', '<a-.>', '<c-w>>')
 map('n', '<a-->', '<c-w>-')
