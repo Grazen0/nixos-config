aebc8bd47dde3a2143f49118291f0a45234d819b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-17 18:33:23 -0500

    feat(neovim): add prose auto text wrapping and de-bloat autocmds

--- configs/home/programs/neovim/config/lua/autocmds.lua
+++ configs/home/programs/neovim/config/lua/autocmds.lua
@@ -1,41 +1,26 @@
-local create_autocmd = vim.api.nvim_create_autocmd
-local create_augroup = vim.api.nvim_create_augroup
-
 -- Highlight yanked text
-create_augroup('YankHighlight', { clear = true })
-create_autocmd('TextYankPost', {
+vim.api.nvim_create_augroup('YankHighlight', { clear = true })
+vim.api.nvim_create_autocmd('TextYankPost', {
   group = 'YankHighlight',
   callback = function()
     vim.highlight.on_yank({ higroup = 'Search', timeout = 500 })
   end,
 })
 
--- Disable formatter + enable conceal for LaTeX files
-create_autocmd('FileType', {
-  pattern = { 'tex', 'plaintex' },
-  callback = function()
-    vim.b.disable_autoformat = true
-    vim.opt_local.conceallevel = 2
-  end,
-})
-
--- Disable wrap for Java files
-create_autocmd('FileType', {
-  pattern = 'java',
-  callback = function() vim.opt_local.wrap = false end,
-})
-
--- Proper syntax highlighting for .inc assembly files
-create_autocmd({ 'BufNew', 'BufNewFile', 'BufRead' }, {
-  pattern = { '*.inc' },
-  callback = function() vim.bo.filetype = 'asm' end,
-})
-
--- Continue lists in Markdown
-create_autocmd('FileType', {
-  pattern = { 'markdown', 'quarto' },
+-- Continue list bullets for some filetypes
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'quarto', 'typst' },
   callback = function()
     vim.opt_local.comments = { 'b:*', 'b:-', 'b:+', 'n:>' }
     vim.opt_local.formatoptions:append('r')
+  end,
+})
+
+-- Text wrapping for prose
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { 'markdown', 'tex', 'plaintex', 'typst' },
+  callback = function()
+    vim.opt_local.textwidth = 80
+    vim.opt_local.formatoptions:append('t')
   end,
 })
