112baa73aafa39761adbac34583139fa3de4f68b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-17 22:44:59 -0500

    fix(takane): remove alt shift toggle and disable spicetify

--- hosts/takane/home.nix
+++ hosts/takane/home.nix
@@ -19,9 +19,8 @@ in
     hyprland.settings.input = {
       kb_layout = mkForce "latam,us";
       kb_variant = mkForce "";
-      kb_options = mkForce "grp:alt_shift_toggle,caps:swapescape,altwin:swap_lalt_lwin";
     };
-    river.settings.keyboard-layout = mkForce "-options 'grp:alt_shift_toggle,caps:swapescape' 'latam,us'";
+    river.settings.keyboard-layout = mkForce "-options 'caps:swapescape' 'latam,us'";
   };
 
   meta.eww.outputs.primary = "BOE";
@@ -29,55 +28,7 @@ in
   programs = {
     foot.settings.main.font = mkForce "${config.theme.font.regular}:size=10.5";
     kitty.font.size = mkForce 10;
-
-    wlogout.layout =
-      let
-        height = 1;
-      in
-      [
-        {
-          label = "lock";
-          action = "loginctl lock-session";
-          text = "Lock";
-          keybind = "l";
-          inherit height;
-        }
-        {
-          label = "hibernate";
-          action = "systemctl hibernate";
-          text = "Hibernate";
-          keybind = "h";
-          inherit height;
-        }
-        {
-          label = "logout";
-          action = "loginctl terminate-user $USER";
-          text = "Logout";
-          keybind = "e";
-          inherit height;
-        }
-        {
-          label = "shutdown";
-          action = "systemctl poweroff";
-          text = "Shutdown";
-          keybind = "s";
-          inherit height;
-        }
-        {
-          label = "suspend";
-          action = "systemctl suspend";
-          text = "Suspend";
-          keybind = "u";
-          inherit height;
-        }
-        {
-          label = "reboot";
-          action = "systemctl reboot";
-          text = "Reboot";
-          keybind = "r";
-          inherit height;
-        }
-      ];
+    spicetify.enable = mkForce false;
   };
 
   home.stateVersion = "24.05";
