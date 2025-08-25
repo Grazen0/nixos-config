6d50cf5b17e89332a3532fab8d4429d337a21996
José Daniel Grayson <josedanielgrayson@proton.me>
2025-08-24 23:16:11 -0500

    refactor: de-bloat fish config a bit

--- configs/home/programs/fish/default.nix
+++ configs/home/programs/fish/default.nix
@@ -24,54 +24,39 @@
     plugins = with pkgs; [
       {
         name = "autopair";
-        src = fishPlugins.autopair;
+        inherit (fishPlugins.autopair) src;
       }
     ];
 
     functions = {
       fish_mode_prompt.body = "";
 
-      __fish_command_not_found_handler = {
-        body = "__fish_default_command_not_found_handler $argv[1]";
-        onEvent = "fish_command_not_found";
-      };
-
       nr = {
         argumentNames = "package";
-        body = "nix run nixpkgs#$package $argv[2..-1]";
+        body = "nix run nixpkgs#$package $argv[2] -- $argv[3..-1]";
       };
     };
 
     interactiveShellInit =
       # fish
       ''
-        # Environment variables
         set -x APHRODITE_THEME_SHOW_TIME yes
-
         source ${inputs.aphrodite-terminal-theme}/fish_prompt.fish
 
-        # Disable greeting message
-        set -U fish_greeting
+        set fish_greeting
 
-        # Vi cursor stuff
         set fish_cursor_default block
         set fish_cursor_insert line
         set fish_cursor_replace_one underscore
         set fish_cursor_visual block
 
-        # Colors
         set fish_color_command green
         set fish_color_error red
         set fish_color_cancel black --background red
         set fish_color_selection brwhite --background ${config.scheme.base02}
 
-        # Bindings
         fish_vi_key_bindings
-        for mode in normal insert
-          bind -M $mode ctrl-space 'accept-autosuggestion'
-          bind -M $mode ctrl-p 'history-prefix-search-backward'
-          bind -M $mode ctrl-n 'history-prefix-search-forward'
-        end
+        bind -M insert ctrl-f 'accept-autosuggestion'
       '';
   };
 }
