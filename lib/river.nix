9218440454123ae7d15c885ded0f690fee4d8d8b
José Daniel Grayson <josedanielgrayson@proton.me>
2025-09-14 03:44:42 -0500

    refactor(lib): remove some unused functions

--- lib/river.nix
+++ lib/river.nix
@@ -1,22 +1,7 @@
 { lib, lib', ... }:
-let
-  inherit (lib) head splitString concatStringsSep;
-in
 rec {
   tagNum = n: lib'.pow 2 (n - 1);
   tagNumStr = n: toString (tagNum n);
 
   spawn = exec: "spawn ${lib.escapeShellArg exec}";
-
-  withRulesLines =
-    rule: actions: exec:
-    let
-      ruleAdds = map (action: "riverctl rule-add ${rule} ${action}") actions;
-      ruleDels = map (action: "riverctl rule-del ${rule} ${head (splitString " " action)}") actions;
-    in
-    ruleAdds ++ [ exec ] ++ ruleDels;
-
-  withRules =
-    rule: actions: exec:
-    concatStringsSep "; " (withRulesLines rule actions exec);
 }
