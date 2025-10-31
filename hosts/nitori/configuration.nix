148a956f72528bd29eb31b686fa5b7d3be71b747
José Daniel Grayson <josedanielgrayson@proton.me>
2025-10-31 12:48:55 -0500

    feat(nitori): add udev rules for vivado

--- hosts/nitori/configuration.nix
+++ hosts/nitori/configuration.nix
@@ -1,4 +1,4 @@
-{ lib', ... }:
+{ lib', pkgs, ... }:
 {
   imports = [
     ../../options/common
@@ -55,6 +55,37 @@
     ATTRS{idVendor}=="0483", ATTRS{idProduct}=="3748", MODE="0666", GROUP="plugdev"
     ATTRS{idVendor}=="0483", ATTRS{idProduct}=="374b", MODE="0666", GROUP="plugdev"
   '';
+
+  services.udev.packages = [
+    (pkgs.writeTextFile {
+      name = "xilinx-dilligent-usb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-digilent-usb.rules";
+      text = ''
+        ATTR{idVendor}=="1443", MODE:="666"
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Digilent", MODE:="666"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "xilinx-pcusb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-pcusb.rules";
+      text = ''
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0008", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0007", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0009", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000d", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="000f", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0013", MODE="666"
+        ATTR{idVendor}=="03fd", ATTR{idProduct}=="0015", MODE="666"
+      '';
+    })
+    (pkgs.writeTextFile {
+      name = "xilinx-ftdi-usb-udev";
+      destination = "/etc/udev/rules.d/52-xilinx-ftdi-usb.rules";
+      text = ''
+        ACTION=="add", ATTR{idVendor}=="0403", ATTR{manufacturer}=="Xilinx", MODE:="666"
+      '';
+    })
+  ];
 
   users.groups.plugdev.members = [ "jdgt" ];
 
