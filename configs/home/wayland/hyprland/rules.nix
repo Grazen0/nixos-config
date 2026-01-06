9e3719f2cbcd5a6a5acbcdc51c30836b883a5395
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-06 16:35:17 -0500

    fix(hyprland): update window rules

--- configs/home/wayland/hyprland/rules.nix
+++ configs/home/wayland/hyprland/rules.nix
@@ -1,7 +1,7 @@
 { config, lib, ... }:
 {
   wayland.windowManager.hyprland.settings = with config.scheme; {
-    windowrulev2 =
+    windowrule =
       let
         dialogTitles = [
           "Open File"
@@ -13,47 +13,47 @@
           "File Upload"
         ];
 
-        dialogRules = map (title: [ "float, title:^(${title})(.*)$" ]) dialogTitles;
+        dialogRules = map (title: [ "match:title ^(${title})(.*)$, float on" ]) dialogTitles;
       in
       [
         # Gives complete control over maximizing
-        "suppressevent maximize, class:.*"
+        "match:class .*, suppress_event maximize"
 
         # Border colors for pinned and monocle windows
-        "bordercolor rgb(${yellow}), fullscreen:1"
+        "match:fullscreen 1, border_color rgb(${yellow})"
 
         # Workspace-specific programs
-        "workspace 7, class:equibop"
-        "workspace 10, class:spotify"
+        "match:class equibop, workspace 7"
+        "match:class spotify, workspace 10"
 
-        "float, class:[Tt]hunar"
-        # "size 960 540, class:[Tt]hunar"
+        "match:class [Tt]hunar, float on"
+        "match:class [Tt]hunar, size 960 540"
 
         # swayimg
-        "size 90% 90%, class:swayimg_(.*)"
-        "float, class:swayimg_(.*)"
-        "center, class:swayimg_(.*)"
+        "match:class swayimg_(.*), size 90% 90%"
+        "match:class swayimg_(.*), float on"
+        "match:class swayimg_(.*), center on"
 
         # pavucontrol
-        "float, class:org\\.pulseaudio\\.pavucontrol"
-        "size 75% 75%, class:org\\.pulseaudio\\.pavucontrol"
+        "match:class org\\.pulseaudio\\.pavucontrol, float on"
+        "match:class org\\.pulseaudio\\.pavucontrol, size 75% 75%"
 
-        "float, class:\\.blueman-manager-wrapped"
-        "float, class:org\\.gnome\\.FileRoller"
-        "float, class:nm-connection-editor"
-        "float, class:nvidia-settings"
-        "float, title:^(Choose one)$"
-        "float, title:^(Steam Settings)$"
-        "float, class:fceux"
-        "size 639 612, class:fceux"
-        "float, class:gemu"
-        "float, class:gtkwave"
+        "match:class \\.blueman-manager-wrapped, float on"
+        "match:class org\\.gnome\\.FileRoller, float on"
+        "match:class nm-connection-editor, float on"
+        "match:class nvidia-settings, float on"
+        "match:title ^(Choose one)$, float on"
+        "match:title ^(Steam Settings)$, float on"
+        "match:class fceux, float on"
+        "match:class fceux, size 639 612"
+        "match:class gemu, float on"
+        "match:class gtkwave, float on"
       ]
       ++ lib.concatLists dialogRules;
 
     layerrule = [
       # fuzzel
-      "dimaround, launcher"
+      "match:namespace rofi, dim_around on"
     ];
   };
 }
