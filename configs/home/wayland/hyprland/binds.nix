b0bd249b82a7d37f85543fe221e0af26c15f1d52
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-19 14:34:40 -0500

    refactor(hyprland): set brightness step to 10%

--- configs/home/wayland/hyprland/binds.nix
+++ configs/home/wayland/hyprland/binds.nix
@@ -155,8 +155,8 @@
           ", XF86AudioNext, exec, ${playerctl} next"
 
           # Brightness control
-          ", XF86MonBrightnessUp, exec, ${brightnessctl} set 5%+"
-          ", XF86MonBrightnessDown, exec, ${brightnessctl} set 5%-"
+          ", XF86MonBrightnessUp, exec, ${brightnessctl} set 10%+"
+          ", XF86MonBrightnessDown, exec, ${brightnessctl} set 10%-"
         ];
 
         bindm = [
