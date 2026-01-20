cc7279f97402b6c0788eb5c924c9c6513fccd932
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-19 21:50:06 -0500

    Revert "fix(neovim): update treesitter config to new version"
    This reverts commit 76d0525e6324a447acf93e51f3e9c011cfeb7b53.

--- configs/home/programs/neovim/config/lua/plugins/lsp/treesitter.lua
+++ configs/home/programs/neovim/config/lua/plugins/lsp/treesitter.lua
@@ -5,11 +5,7 @@ return {
     build = ':TSUpdate',
     config = function(_, opts)
       vim.api.nvim_create_autocmd({ 'BufReadPost', 'BufNewFile' }, {
-        callback = function()
-          if vim.tbl_contains(require('nvim-treesitter').get_installed(), vim.bo.filetype) then
-            vim.treesitter.start()
-          end
-        end,
+        callback = function() vim.treesitter.start() end,
       })
     end,
   },
