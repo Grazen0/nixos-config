787873025a8382628de51f3084b11e6917edc327
José Daniel Grayson <josedanielgrayson@proton.me>
2026-01-21 09:40:17 -0500

    feat(takane): enable some power management options

--- components/nixos/laptop/default.nix
+++ components/nixos/laptop/default.nix
@@ -1,11 +1,15 @@
 {
   powerManagement.enable = true;
+  powerManagement.cpuFreqGovernor = "ondemand";
 
   networking.networkmanager.wifi.powersave = true;
 
   services = {
     thermald.enable = true;
+    tlp.enable = true;
     auto-cpufreq.enable = true;
     upower.enable = true;
   };
+
+  powerManagement.powertop.enable = true;
 }
