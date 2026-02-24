d805661a2126de0e8111f2e0cf257a1381ca55e6
José Daniel Grayson <josedanielgrayson@proton.me>
2026-02-23 19:48:40 -0500

    fix(sddm): fix where-is-my-sddm-theme with qtgraphicaleffects package

--- configs/nixos/services/sddm/default.nix
+++ configs/nixos/services/sddm/default.nix
@@ -1,44 +1,37 @@
 {
   config,
-  lib,
   pkgs,
   ...
 }:
-let
-  inherit (config.services.displayManager) sddm;
-in
 {
-  services.xserver = lib.mkIf sddm.enable {
-    enable = true;
-  };
-
-  environment.systemPackages = lib.optionals sddm.enable [
-    (pkgs.where-is-my-sddm-theme.override {
-      variants = [ "qt5" ];
-      themeConfig.General = with config.scheme.withHashtag; {
-        font = config.theme.font.propo;
-        helpFont = config.theme.font.propo;
-
-        backgroundFill = base01;
-        passwordInputBackground = base00;
-        passwordCursorColor = base05;
-        passwordTextColor = base05;
-        basicTextColor = base05;
-
-        passwordInputWidth = 0.2;
-
-        passwordFontSize = 16;
-        sessionsFontSize = 14;
-        usersFontSize = 14;
-        helpFontSize = 14;
-
-        showSessionsByDefault = true;
-        showUsersByDefault = true;
-      };
-    })
-  ];
-
   services.displayManager.sddm = {
-    theme = "where_is_my_sddm_theme_qt5";
+    wayland.enable = true;
+    theme = "where_is_my_sddm_theme";
+
+    extraPackages = with pkgs; [
+      kdePackages.qt5compat
+      (where-is-my-sddm-theme.override {
+        themeConfig.General = with config.scheme.withHashtag; {
+          font = config.theme.font.propo;
+          helpFont = config.theme.font.propo;
+
+          backgroundFill = base01;
+          passwordInputBackground = base00;
+          passwordCursorColor = base05;
+          passwordTextColor = base05;
+          basicTextColor = base05;
+
+          passwordInputWidth = 0.2;
+
+          passwordFontSize = 16;
+          sessionsFontSize = 14;
+          usersFontSize = 14;
+          helpFontSize = 14;
+
+          showSessionsByDefault = true;
+          showUsersByDefault = true;
+        };
+      })
+    ];
   };
 }
