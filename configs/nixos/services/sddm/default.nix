45b39d36d8e45a300d08ab9ad0e75a0a278d6ce3
José Daniel Grayson <josedanielgrayson@proton.me>
2026-03-16 13:15:17 -0500

    fix(sddm): fix where-is-my-sddm-theme

--- configs/nixos/services/sddm/default.nix
+++ configs/nixos/services/sddm/default.nix
@@ -4,34 +4,37 @@
   ...
 }:
 {
+  environment.systemPackages = with pkgs; [
+    (where-is-my-sddm-theme.override {
+      themeConfig.General = with config.scheme.withHashtag; {
+        font = config.theme.font.propo;
+        helpFont = config.theme.font.propo;
+
+        backgroundFill = base01;
+        passwordInputBackground = base00;
+        passwordCursorColor = base05;
+        passwordTextColor = base05;
+        basicTextColor = base05;
+
+        passwordInputWidth = 0.2;
+
+        passwordFontSize = 16;
+        sessionsFontSize = 14;
+        usersFontSize = 14;
+        helpFontSize = 14;
+
+        showSessionsByDefault = true;
+        showUsersByDefault = true;
+      };
+    })
+  ];
+
   services.displayManager.sddm = {
     wayland.enable = true;
     theme = "where_is_my_sddm_theme";
 
     extraPackages = with pkgs; [
       kdePackages.qt5compat
-      (where-is-my-sddm-theme.override {
-        themeConfig.General = with config.scheme.withHashtag; {
-          font = config.theme.font.propo;
-          helpFont = config.theme.font.propo;
-
-          backgroundFill = base01;
-          passwordInputBackground = base00;
-          passwordCursorColor = base05;
-          passwordTextColor = base05;
-          basicTextColor = base05;
-
-          passwordInputWidth = 0.2;
-
-          passwordFontSize = 16;
-          sessionsFontSize = 14;
-          usersFontSize = 14;
-          helpFontSize = 14;
-
-          showSessionsByDefault = true;
-          showUsersByDefault = true;
-        };
-      })
     ];
   };
 }
