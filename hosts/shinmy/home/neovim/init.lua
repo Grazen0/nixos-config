a81e7bd64c625e3484c9b22c894fda4b09e6cc58
José Daniel Grayson <josedanielgrayson@proton.me>
2026-03-19 20:03:40 -0500

    refactor(shinmy): set neovim foldmethod to expr on lsp attach

--- hosts/shinmy/home/neovim/init.lua
+++ hosts/shinmy/home/neovim/init.lua
@@ -296,6 +296,7 @@ vim.api.nvim_create_autocmd('LspAttach', {
 
     if client and client:supports_method('textDocument/foldingRange') then
       local win = vim.api.nvim_get_current_win()
+      vim.wo[win][0].foldmethod = 'expr'
       vim.wo[win][0].foldexpr = 'v:lua.vim.lsp.foldexpr()'
     end
   end,
