679c732542a84451ec0f0e3fe0617bbd3febb2b6
José Daniel Grayson <josedanielgrayson@proton.me>
2026-05-13 11:41:10 -0500

    refactor(neovim): disable auto list bullets and auto-wrap

--- configs/home/programs/neovim/config/init.lua
+++ configs/home/programs/neovim/config/init.lua
@@ -102,24 +102,6 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
--- Continue list bullets for some filetypes
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'markdown', 'quarto', 'typst' },
-  callback = function()
-    vim.opt_local.comments = { 'b:*', 'b:-', 'b:+', 'n:>' }
-    vim.opt_local.formatoptions:append('r')
-  end,
-})
-
--- Text wrapping for prose
-vim.api.nvim_create_autocmd('FileType', {
-  pattern = { 'markdown', 'tex', 'plaintex', 'typst' },
-  callback = function()
-    vim.opt_local.textwidth = 80
-    vim.opt_local.formatoptions:append('t')
-  end,
-})
-
 vim.api.nvim_create_autocmd('QuickFixCmdPre', {
   command = 'wall',
 })
