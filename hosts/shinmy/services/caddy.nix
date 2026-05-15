12c856ea194bf92e7e1053626a07b1d5f156c2cb
José Daniel Grayson <josedanielgrayson@proton.me>
2026-05-15 00:02:37 -0500

    chore(shinmy): update caddy-dns/cloudflare hash

--- hosts/shinmy/services/caddy.nix
+++ hosts/shinmy/services/caddy.nix
@@ -76,7 +76,7 @@ in
     enable = true;
     package = pkgs.caddy.withPlugins {
       plugins = [ "github.com/caddy-dns/cloudflare@v0.2.1" ];
-      hash = "sha256-Zls+5kWd/JSQsmZC4SRQ/WS+pUcRolNaaI7UQoPzJA0=";
+      hash = "sha256-hEIqK6F+9OCcd4JueVSidfUgQsVPWo0/imciD1UnqRo=";
     };
 
     configFile = config.sops.templates."Caddyfile".path;
