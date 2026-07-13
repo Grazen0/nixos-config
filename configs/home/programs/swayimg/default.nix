5809d8e19b61bcab8efdd1ea389f4ddbf3ea8a28
José Daniel Grayson <josedanielgrayson@proton.me>
2026-07-13 14:22:43 -0500

    fix(swayimg): update config to lua

--- configs/home/programs/swayimg/default.nix
+++ configs/home/programs/swayimg/default.nix
@@ -1,57 +1,76 @@
 { config, ... }:
 {
   programs.swayimg = {
-    settings = with config.scheme.withHashtag; {
-      viewer = {
-        window = base01;
-        scale = "fit";
-        antialiasing = "bicubic";
-      };
+    initLua =
+      with config.scheme;
+      # lua
+      ''
+        swayimg.enable_antialiasing(true)
+        swayimg.enable_decoration(true)
+        swayimg.enable_overlay(false)
+        swayimg.enable_exif_orientation(true)
 
-      list.all = "yes";
+        swayimg.viewer.set_default_scale('optimal')
+        swayimg.viewer.set_window_background(0xff${base00})
 
-      gallery = {
-        window = base00;
-        antialiasing = "mks13";
-      };
+        swayimg.text.set_font('${config.theme.font.propo}')
+        swayimg.text.set_size(18)
+        swayimg.text.set_foreground(0xff${base05})
+        swayimg.text.set_shadow(0xff${base01})
 
-      font = {
-        name = config.theme.font.propo;
-        size = 18;
-        color = base05;
-        shadow = base01;
-      };
+        swayimg.imagelist.enable_adjacent(true)
+        swayimg.imagelist.enable_fsmon(true)
 
-      info.show = "no";
+        swayimg.text.hide()
 
-      "keys.viewer" = {
-        p = "prev_file";
-        n = "next_file";
+        swayimg.viewer.on_key('p', function()
+          swayimg.viewer.switch_image('prev')
+        end)
 
-        g = "first_file";
-        "Shift+g" = "last_file";
+        swayimg.viewer.on_key('n', function()
+          swayimg.viewer.switch_image('next')
+        end)
 
-        h = "step_left 10";
-        j = "step_down 10";
-        k = "step_up 10";
-        l = "step_right 10";
+        swayimg.viewer.on_key('g', function()
+          swayimg.viewer.switch_image('first')
+        end)
 
-        "Shift+j" = "zoom -10";
-        "Shift+k" = "zoom +10";
-      };
+        swayimg.viewer.on_key('Shift+g', function()
+          swayimg.viewer.switch_image('last')
+        end)
 
-      "keys.gallery" = {
-        g = "first_file";
-        "Shift+g" = "last_file";
+        local step_x = function(dist)
+          return function()
+            local wnd = swayimg.get_window_size()
+            local pos = swayimg.viewer.get_position()
+            swayimg.viewer.set_abs_position(math.floor(pos.x + (wnd.width / dist)), pos.y);
+          end
+        end
 
-        h = "step_left";
-        j = "step_down";
-        k = "step_up";
-        l = "step_right";
+        local step_y = function(dist)
+          return function()
+            local wnd = swayimg.get_window_size()
+            local pos = swayimg.viewer.get_position()
+            swayimg.viewer.set_abs_position(pos.x, math.floor(pos.y + (wnd.height / dist)));
+          end
+        end
 
-        "Shift+j" = "page_down";
-        "Shift+k" = "page_up";
-      };
-    };
+        swayimg.viewer.on_key("h", step_x(10))
+        swayimg.viewer.on_key("j", step_y(-10))
+        swayimg.viewer.on_key("k", step_y(10))
+        swayimg.viewer.on_key("l", step_x(-10))
+
+        swayimg.viewer.on_key("Shift+k", function()
+          local scale = swayimg.viewer.get_scale()
+          scale = scale + scale / 10
+          swayimg.viewer.set_abs_scale(scale);
+        end)
+
+        swayimg.viewer.on_key("Shift+j", function()
+          local scale = swayimg.viewer.get_scale()
+          scale = scale - scale / 10
+          swayimg.viewer.set_abs_scale(scale);
+        end)
+      '';
   };
 }
